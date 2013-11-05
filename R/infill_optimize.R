b70180642c23f41f3a0ebe4518ee744de183ce18
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-11-06 00:11:00 +0100

    add restarts to simple ES infill optimizer

--- R/infill_optimize.R
+++ R/infill_optimize.R
@@ -120,6 +120,7 @@ infillOptCMAES = function(infill.crit, model, control, par.set, opt.path, design
 # compare more honestly
 
 #FIXME potentially allow more than 1 kid
+#FIXME check what this optimizer can do in checkStuff and allow rounding of integers
 infillOptSimpleES = function(infill.crit, model, control, par.set, opt.path, design) {
   requirePackages("emoa", why="infillOptSimpleES")
 
@@ -132,27 +133,44 @@ infillOptSimpleES = function(infill.crit, model, control, par.set, opt.path, des
   crossover = sbx_operator(control$infill.opt.es.eta, control$infill.opt.es.p,
     getLower(par.set), getUpper(par.set))
 
-  # Random inital population:
-  X = generateDesign(mu, par.set, fun=randomLHS)
-  y = infill.crit(X, model, control, par.set, design)
+  best.x = NULL
+  best.y = Inf
 
-  for (i in 1:control$infill.opt.es.maxit) {
-    # Create new individual (mu + 1)
-    parents = sample(1:mu, 2)
-    # get two kids from CX, sel. 1 randomly, mutate
-    child = crossover(t(X[parents, , drop=FALSE]))
-    child1 = child[,sample(c(1, 2), 1)]
-    child1 = mutate(child1)
-    # Add new individual:
-    X[nrow(X) + 1,] = child1
-    child2 = setColNames(as.data.frame(as.list(child1)), repids)
-    y[length(y) + 1] = infill.crit(child2, model, control, par.set, design)
+  for (restart.iter in 1:control$infill.opt.restarts) {
 
-    # get element we want to remove from current pop
-    to.kill = getMaxIndex(y, ties.method="random")
-    X = X[-to.kill, ,drop=FALSE]
-    y = y[-to.kill]
+    # random inital population:
+    X = generateDesign(mu, par.set, fun=randomLHS)
+    y = infill.crit(X, model, control, par.set, design)
+
+    for (i in 1:control$infill.opt.es.maxit) {
+      # Create new individual (mu + 1)
+      parents = sample(1:mu, 2)
+      # get two kids from CX, sel. 1 randomly, mutate
+      child = crossover(t(X[parents, , drop=FALSE]))
+      child1 = child[,sample(c(1, 2), 1)]
+      child1 = mutate(child1)
+      # Add new individual:
+      X[nrow(X) + 1,] = child1
+      child2 = setColNames(as.data.frame(as.list(child1)), repids)
+      y[length(y) + 1] = infill.crit(child2, model, control, par.set, design)
+
+      # get element we want to remove from current pop
+      to.kill = getMaxIndex(y, ties.method="random")
+      X = X[-to.kill, ,drop=FALSE]
+      y = y[-to.kill]
+    }
+    select = getMinIndex(y)
+    y1 = y[select] 
+    x1 = X[select, , drop=FALSE]
+    if (y1 < best.y) {
+      best.x = x1
+      best.y = y1
+    }
+    rownames(best.x) = NULL
+    return(best.x)
   }
-  rownames(X) = NULL
-  X[getMinIndex(y), ,drop=FALSE]
 }
+
+
+
+
--- inst/tests/test_infill_opt_es.R
+++ inst/tests/test_infill_opt_es.R
@@ -7,7 +7,7 @@ test_that("infillopt es", {
   ps = makeNumericParamSet(lower=lower_bounds(objfun), upper=upper_bounds(objfun))
 
   ctrl = makeMBOControl(init.design.points=20, iters=5, propose.points=1, 
-    infill.crit="ei", infill.opt="es",
+    infill.crit="ei", infill.opt="es", infill.opt.restarts=2L,
     infill.opt.es.maxit=200, infill.opt.es.mu=20L, infill.opt.es.p=0.5, infill.opt.es.eta=10)
 
   lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
