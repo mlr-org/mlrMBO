e100a603f1c7c353b2c2a8928bcd5ea8f4006e69
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-11-10 20:03:54 +0100

    focus search was severly broken, fixed

--- R/infill_opt_focus.R
+++ R/infill_opt_focus.R
@@ -7,7 +7,6 @@
 #FIXME should we shrink if a local value is NA (dependent param)
 
 infillOptFocus = function(infill.crit, model, control, par.set, opt.path, design) {
-  global.x = NULL
   global.y = Inf
 
   # restart the whole crap some times
@@ -18,51 +17,64 @@ infillOptFocus = function(infill.crit, model, control, par.set, opt.path, design
       newdesign = generateDesign(control$infill.opt.random.points, par.set,
         randomLHS, ints.as.num=TRUE, logicals.as.factor=TRUE)
       y = infill.crit(imputeFeatures(newdesign, par.set, control), model, control, par.set, design)
+      
       # get current best value
       local.index = getMinIndex(y, ties.method="random")
       local.y = y[local.index]
-      local.x = newdesign[local.index, , drop=FALSE]
+      local.x.df = newdesign[local.index, , drop=FALSE]
+      local.x.list = dfRowToList(recodeTypes(local.x.df, par.set), par.set, 1)
+      
       # if we found a new best value, store it
       if (local.y < global.y) {
-        global.x = local.x
+        global.x.df = local.x.df
         global.y = local.y
       }
 
       # now shrink par.set object so we search more locally
       for (i in seq_along(par.set$pars)) {
         par = par.set$pars[[i]]
-        val = local.x[[i]]
-        if (par$type %in% c("numeric", "integer", "numericvector", "integervector")) {
-          # shrink to range / 2, centered at val
-          range = par$upper - par$lower
-          par$lower = pmax(par$lower, val - range/4)
-          par$upper = pmin(par$upper, val + range/4)
-          if (par$type %in% c("integer", "integervector")) {
-            par$lower = floor(par$lower)
-            par$upper = ceiling(par$upper)
-          }
-        } else if (par$type %in% c("discrete", "discretevector")) {
-          # randomly drop a level, which is not val
-          if (length(par$values) > 1L) {
-            val.names = names(par$values)
-            # remove current val from delete options, should work also for NA
-            val.names = setdiff(val.names, val)
-            to.del = sample(seq_along(val.names), 1)
-            par$values = par$values[-i]
+        val = local.x.list[[par$id]]
+        # only shrink when there is a value
+        if (!isScalarNA(val)) {
+          if (par$type %in% c("numeric", "integer", "numericvector", "integervector")) {
+            # shrink to range / 2, centered at val
+            range = par$upper - par$lower
+            par$lower = pmax(par$lower, val - range/4)
+            par$upper = pmin(par$upper, val + range/4)
+            if (par$type %in% c("integer", "integervector")) {
+              par$lower = floor(par$lower)
+              par$upper = ceiling(par$upper)
+            }
+          } else if (par$type %in% c("discrete", "discretevector")) {
+            # randomly drop a level, which is not val
+            if (length(par$values) > 1L) {
+              val.names = names(par$values)
+              # remove current val from delete options, should work also for NA
+              val.names = setdiff(val.names, val)
+              to.del = sample(seq_along(val.names), 1)
+              par$values = par$values[-i]
+            }
           }
         }
         par.set$pars[[i]] = par
       }
     }
   }
-  # convert back to correct type
-  types = unlist(lapply(par.set$pars, function(p) rep(p$type, p$len)))
-  for (i in seq_col(global.x)) {
-    if (types[i] %in% c("integer", "integervector"))
-      global.x[,i] = as.integer(global.x[,i])
-    else if (types[i] %in% c("logical", "logicalvector"))
-      global.x[,i] = as.logical(as.character(global.x[,i]))
-  }
-  global.x
+  recodeTypes(global.x.df, par.set)
 }
+
+# as we operate on other types for the learner (ints are nums, logs are factors),
+# we have to convert back sometimes for dfRowsToList to work
+recodeTypes = function(df, par.set) {
+  types = unlist(lapply(par.set$pars, function(p) rep(p$type, p$len)))
+  for (i in seq_col(df)) {
+    if (types[i] %in% c("integer", "integervector"))
+      df[,i] = as.integer(df[,i])
+    else if (types[i] %in% c("logical", "logicalvector"))
+      df[,i] = as.logical(as.character(df[,i]))
+  }
+  return(df)
+}
+
+
 
