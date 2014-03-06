237ebc2a6cf6cebfc29fa95442540c24ab057ab6
Daniel Horn <daniel.horn@tu-dortmund.de>
2014-03-06 16:26:31 +0100

    New method to sample a set of weighting vectors for parEGO

--- todo-files/runParego.R
+++ todo-files/runParego.R
@@ -7,11 +7,11 @@ par.set = makeParamSet(
   )
 learner = makeLearner("regr.km")
 control = makeMBOControl(number.of.targets = 2L, iters = 5L, parEGO.s = 100L,
-  parEGO.multipoint.number = 4L, parEGO.source.file = "todo-files/runParEgoSourceFiles.R")
-funs = list(fun1 = function(x) {Sys.sleep(1); zdt1(x$x)},
-            fun2 = function(x) {Sys.sleep(1); lz1(x$x)})
-parallelStartSocket(cpus = 4)
-for(fun in funs) {
+  parEGO.multipoint.number = 20L, parEGO.source.file = "todo-files/runParEgoSourceFiles.R")
+funs = list(fun1 = function(x) { zdt1(x$x)},
+            fun2 = function(x) { lz1(x$x)})
+parallelStartSocket(cpus = 2)
+for(fun in funs[1]) {
   resMBO = mboParEGO(fun, par.set, learner = learner, control = control)
   plot(resMBO$pareto.front, xlim = c(0, 2), ylim = c(0, 2))
   curve(1 - sqrt(x), add = TRUE)
