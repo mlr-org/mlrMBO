e0bab5ed528c35f414490dc8abce532c10ad308a
Daniel Horn <daniel.horn@tu-dortmund.de>
2014-03-31 11:54:01 +0200

    And now, as expected, the test fails ...

--- inst/tests/test_different_learners.R
+++ inst/tests/test_different_learners.R
@@ -1,12 +1,20 @@
 context("different learner")
 
-test_that("infill optimizers", {
+test_that("mbo works with different learners", {
   
-  f = function(x) if (x$x3 == "a") x$x1^2 + x$x2^2 else 5
+  ## This function works, the other does not ..???
+  #f = function(x) if (x$x3 == "a") x$x1^2 + x$x2^2 else 5
+  #ps = makeParamSet(
+  #  makeNumericParam("x1", lower = -2, upper = 1),
+  #  makeNumericParam("x2", lower = -1, upper = 2),
+  #  makeDiscreteParam("x3", values = c("a", "b"))
+  #)
+  
+  f = function(x)
+    ifelse(x$disc1 == "a", x$num1 * 2 - 1, 1 - x$num1)
   ps = makeParamSet(
-    makeNumericParam("x1", lower = -2, upper = 1),
-    makeNumericParam("x2", lower = -1, upper = 2),
-    makeDiscreteParam("x3", values = c("a", "b"))
+    makeDiscreteParam("disc1", values = c("a", "b")),
+    makeNumericParam("num1", lower = 0, upper = 1)
   )
   
   ctrl = makeMBOControl(iters = 2, init.design.points = 10,
