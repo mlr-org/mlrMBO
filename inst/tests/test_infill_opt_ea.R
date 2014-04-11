9296e45735f2c72e065e8db860250df5e3a5f151
Jakob Bossek <info@jakobbossek.de>
2014-04-11 21:05:40 +0200

    make test faster by reducing the number of iteration form 5 to 4

--- inst/tests/test_infill_opt_ea.R
+++ inst/tests/test_infill_opt_ea.R
@@ -6,7 +6,7 @@ test_that("infillopt ea", {
 
   ps = makeNumericParamSet(len=2L, lower=-5, upper=5)
 
-  ctrl = makeMBOControl(init.design.points=20, iters=5, propose.points=1,
+  ctrl = makeMBOControl(init.design.points=20, iters=4, propose.points=1,
     infill.crit="ei", infill.opt="ea", infill.opt.restarts=2L,
     infill.opt.ea.maxit=75, infill.opt.ea.lambda = 1L)
 
