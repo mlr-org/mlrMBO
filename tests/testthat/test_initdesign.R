f5846746f64c69d55c8da0fe3b44549cfdb4e2e4
Bernd Bischl <you@example.com>
2014-06-13 23:22:39 +0200

    refactor + clean up

--- tests/testthat/test_initdesign.R
+++ tests/testthat/test_initdesign.R
@@ -1,24 +1,41 @@
 context("init design")
 
-test_that("mbo works with init design", {
-  objfun = function(x) x^2
+test_that("init design", {
+  f = makeMBOFunction(function(x) sum(x^2))
 
-  ps = makeNumericParamSet(len = 1, lower = -5, upper = 5)
-
-  design.x = generateDesign(5, ps)
-  y = apply(design.x, 1, objfun)
-  design = cbind(design.x, y)
-  attr(design, "trafo") = FALSE
-  learner_km = makeLearner("regr.km", predict.type = "se", covtype = "matern3_2", nugget.estim = TRUE)
-
-  ctrl = makeMBOControl(
-    iters = 5,
-    infill.crit = "ei",
-    init.design.points = 10,
-    infill.opt = "focussearch"
+  # without trafo
+  ps = makeParamSet(
+    makeNumericParam("x1", lower = -2, upper = 1),
+    makeNumericParam("x2", lower = -1, upper = 2)
   )
 
-  or = mbo(makeMBOFunction(objfun), design = design, par.set = ps, learner = learner_km, control = ctrl, show.info =TRUE)
-  expect_true(!is.null(or))
+  learner = makeLearner("regr.km", nugget.estim = TRUE)
+  ctrl = makeMBOControl(iters = 1, infill.opt.focussearch.points = 10)
+
+  des = generateDesign(10, par.set = ps)
+
+  or = mbo(f, ps, des, learner, ctrl)
+  expect_equal(getOptPathLength(or$opt.path), 11L)
+  expect_true(!any(is.na(or$y)))
+
+  # with precomputed y
+  des$y = apply(des, 1, f)
+  or = mbo(f, ps, des, learner, ctrl)
+  expect_equal(getOptPathLength(or$opt.path), 11L)
+  expect_true(!any(is.na(or$y)))
+
+
+  # with trafo
+  ps = makeParamSet(
+    makeNumericParam("x1", lower = -2, upper = 1),
+    makeNumericParam("x2", lower = -1, upper = 2, trafo = function(x) x/2)
+  )
+
+  des = generateDesign(10, ps, trafo = TRUE)
+  expect_error(mbo(f, ps, des, learner, ctrl))
+
+  # wrong col names in inir design
+  des = data.frame(x1 = c(-0.49, 0.24, -1.47), z = c(-0.70, 1.29, 0.73))
+  expect_error(mbo(f, ps, des, learner, ctrl))
 })
 
