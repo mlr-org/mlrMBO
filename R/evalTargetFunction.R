2f495a5d39176d70299466b24fac1ab09605fa9c
Bernd Bischl <you@example.com>
2014-06-13 21:03:06 +0200

    refactor + clean up

--- R/evalTargetFunction.R
+++ R/evalTargetFunction.R
@@ -1,93 +1,81 @@
 # Evaluates target fitness function on given set of points.
 #
-# params fun, par.set, opt.path, control, show.info and more.args as usual
+# xs = list of points
+# dobs = dobs values for xs, same len or 1 int
+# returns numeric-vector
 #
-# @param xs [\code{list}]\cr
-#   Set of points on which fun shall be evaluated.
-# @param oldopts [\code{list}]\cr
-#   Old mlr configuration.
-# @param more.args [\code{list}]\cr
-#   Further arguments passed to fitness function.
-# @return [\code{list}]:
-#   \item{ys}{Vector or matrix of objective values. First dim = length(xs).}
-#   \item{times}{Vector of times it took to evaluate the objective).}
-evalTargetFun = function(fun, par.set, xs, opt.path, control, show.info, oldopts, more.args = list()) {
-  xs = lapply(xs, trafoValue, par=par.set)
-  fun2 = function(x) {
+# - trafo xs
+# - evals all xs, measures time
+# - potentially imputes errors, NAs, NaNs, Infs
+#
+evalTargetFun = function(fun, par.set, dobs, xs, opt.path, control, show.info, oldopts,
+  more.args = list(), extras) {
+
+  # short names and so on
+  y.name = control$y.name
+  nevals = length(xs)
+  ny = control$number.of.targets
+  num.format = control$output.num.format
+  num.format.string = paste("%s = ", num.format, sep = "")
+  dobs = ensureVector(dobs, n = nevals, cl = "integer")
+
+  # trafo
+  xs = lapply(xs, trafoValue, par = par.set)
+
+  # function to measure of fun call
+  wrapfun = function(x) {
     st = proc.time()
-    y = do.call(fun, insert(list(x=x), more.args))
+    y = do.call(fun, insert(list(x = x), more.args))
     st = proc.time() - st
-    if (length(y) != control$number.of.targets)
-      stopf("Objective function output has wrong length: %i. Should be %i.",
-        length(y), control$number.of.targets)
-    return(list(y = y, time = st[3]))
+    return(list(y = y, time = st[3], err = NA_character_))
   }
   # restore mlr configuration
   configureMlr(on.learner.error = oldopts[["ole"]], show.learner.output = oldopts[["slo"]])
-  
+
   # If an error occurs in f and we don't want to impute, stop - in this case
   # parallelMap gets NULL as impute function. In this case, execution will
   # stop at this line. Otherwise we use the identity for imputation - now
   # every element in z could be an error object.
-  z = parallelMap(fun2, xs, impute.error = if(control$impute.errors) identity else NULL)
-  # Check for errors - impute them with NA and save the error message
-  error.messages = character(length(z))
-  for (index in seq_along(z)) {
-    if(is.error(z[[index]])) {
-      error.messages[index] = z[[index]]$message
-      z[[index]] = list(y = rep(NA_real_, control$number.of.targets), time = NA_real_)
-    }
-  }
-  
-  # ys is row-matrix with number.of.targets cols
-  ys = if (control$number.of.targets == 1L)
-    matrix(extractSubList(z, "y"), ncol = 1L)
-  else
-    setColNames(extractSubList(z, "y", simplify = "rows"), control$y.name)
-  times = extractSubList(z, "time")
+  res = parallelMap(wrapfun, xs, impute.error = if (control$impute.errors) identity else NULL)
 
-  # show some info on the console
-  if (show.info) {
-    num.format = control$output.num.format
-    num.format.string = paste("%s=", num.format, sep="")
-    dob = opt.path$env$dob
-    dob = if (length(dob) == 0L) 0 else max(dob) + 1
-    for (ind in seq_along(xs)) {
-      #FIXME: show time?
-      messagef("[mbo] %i: %s : %s", dob, paramValueToString(par.set, xs[[ind]], num.format = num.format),
-        paste(sprintf(num.format.string, control$y.name, ys[ind, ]), collapse = ", "))
+  # loop evals and to some post-processing
+  for (i in 1:nevals) {
+    r = res[[i]]; x = xs[[i]]; dob = dobs[i]
+
+    if (is.error(r)) {
+      # if error produce fake result list
+      res[[i]] = list(y = rep(NA_real_, ny), time = NA_real_, err = r$message)
+    } else {
+      if (!(is.numeric(r$y) && length(r$y) == ny))
+        stopf("Objective function output must be a numeric of length: %i!", ny)
     }
+    y = r$y
+    # are there NAs or NANs in the output? if so, produce custom error and impute with user fun
+    isna = is.na(y) | is.nan(y) | is.infinite(y)
+    if (any(isna)) {
+      r$err = sprintf("mlrMBO: NA or NaN in objective output and imputed: %s",
+        collapsef("%s = %g", y.name, y), sep = ", ")
+      y[isna] = mapply(which(isna), function(d)
+        control$impute[[d]], x, y[d], MoreArgs = list(opt.path = opt.path))
+    }
+
+    if (show.info) { # show info on console
+      #FIXME: show time?
+      messagef("[mbo] %i: %s : %s", dob, paramValueToString(par.set, x, num.format = num.format),
+        collapse(sprintf(num.format.string, y.name, y), ", "))
+    }
+    # log to opt path
+    addOptPathEl(opt.path, x = x, y = r$y, dob = dob,
+      error.message = r$err, exec.time = r$time, extra = extras[[i]])
   }
-  
+
   # FIXME: Do we need this here? This function is for function evaluation,
   # we should not need to configure mlr in this function.
+  # BB: there is the problem if we call an mlr learner in the target function.....
+  # BB: still, this is bad style here
   configureMlr(on.learner.error = control$on.learner.error,
     show.learner.output = control$show.learner.output)
-  
-  # Generate impute-error-messages for the opt-path
-  for (ind in seq_len(nrow(ys))) {
-    na.inds = is.na(ys[ind, ])
-    if (any(na.inds)) {
-      new.error.message = paste("mlrMBO: NAs detected and imputed for: ",
-        collapse(control$y.name[is.na(ys[ind, ])], sep = ", "), sep = "")
-      if (error.messages[ind] == "")
-        error.messages[ind] = new.error.message
-      else
-        error.messages[ind] = paste(new.error.message, ". Original error message from target function: ",
-          error.messages[ind])
-    }
-  }
-  
-  # Now impute every NA with the given imputation function
-  for (dim in seq_len(control$number.of.targets)) {
-    # impute all broken y values in ys by imputation function of user
-    broken = which(is.na(ys[, dim]) | is.nan(ys[, dim]) | is.infinite(ys[, dim]))
-    if (length(broken) > 0L) {
-      ys[broken, dim] = mapply(control$impute[[dim]], xs[broken], ys[broken, dim],
-        MoreArgs = list(opt.path = opt.path), USE.NAMES = FALSE)
-    }
-  }
-  
-  return(list(ys = ys, times = times, error.messages = error.messages))
+
+  invisible(NULL)
 }
 
