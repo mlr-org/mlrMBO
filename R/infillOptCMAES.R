f5e2d2ed09473dd51156578d42a767ece65c5f62
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-15 08:17:26 +0200

    clean up

--- R/infillOptCMAES.R
+++ R/infillOptCMAES.R
@@ -4,8 +4,6 @@
 #
 # @param infill.crit [\code{function}]\cr
 #   Infill criterion function.
-# @param design [\code{data.frame}]\cr
-#   Design of already visited points.
 # @param model [\code{\link{WrappedModel}}]\cr
 #   Model fitted on design.
 # @param control [\code{\link{MBOControl}}]\cr
@@ -14,6 +12,8 @@
 #   Parameter set.
 # @param opt.path [\code{\link[ParamHelpers{OptPath}}]\cr
 #   Optimization path / archive.
+# @param design [\code{data.frame}]\cr
+#   Design of already visited points.
 # @param ... [\code{ANY}]\cr
 #   Additional arguments passed to \code{infill.crit}.
 # @return [\code{data.frame}]. One proposed point that should be evaluated.
@@ -29,14 +29,14 @@ infillOptCMAES = function(infill.crit, model, control, par.set, opt.path, design
   low = getLower(par.set)
   upp = getUpper(par.set)
 
-  rep.pids = getParamIds(par.set, repeated=TRUE, with.nr=TRUE)
+  rep.pids = getParamIds(par.set, repeated = TRUE, with.nr = TRUE)
   #eval all points of 1 generation at once
   cmaes.control = control$cmaes.control
   cmaes.control$vectorized = TRUE
   f = function(x) {
     newdata = as.data.frame(t(x))
     colnames(newdata) = rep.pids
-    infill.crit(newdata, model, control, par.set, design)
+    infill.crit(newdata, model, control, par.set, design, ...)
   }
 
   results = vector("list", control$infill.opt.restarts)
@@ -48,11 +48,20 @@ infillOptCMAES = function(infill.crit, model, control, par.set, opt.path, design
       start = sampleValue(par.set)
     }
     start = unlist(start)
-    results[[i]] = cma_es(par=start, fn=f, lower=low, upper=upp, control=control$infill.opt.cmaes.control)
+    results[[i]] = cma_es(par = start, fn = f, lower = low, upper = upp, control = control$infill.opt.cmaes.control)
   }
   ys = extractSubList(results, "value")
-  j = which(rank(ys, ties.method="random") == 1L)
-  as.data.frame(t(results[[j]]$par))
+  ys = ys[!is.infinite(ys)]
+  res = NULL
+  # all CMA-ES runs failed. Therefore we sample a random point and warn
+  if (length(ys) == 0) {
+    res = t(sampleValue(par.set))
+    warningf("Infill optimizer CMA-ES crashed %i times. Random point generated instead.", control$infill.opt.restart)
+  } else {
+    j = which(rank(ys, ties.method = "random") == 1L)
+    res = t(results[[j]]$par)
+  }
+  setColNames(as.data.frame(res), rep.pids)
 }
 
 # FIXME: allow DiceOptim optimizer later...
@@ -61,10 +70,10 @@ infillOptCMAES = function(infill.crit, model, control, par.set, opt.path, design
 #   low = getLower(par.set)
 #   upp = getUpper(par.set)
 #
-#   i = getOptPathBestIndex(opt.path, ties="random")
+#   i = getOptPathBestIndex(opt.path, ties = "random")
 #   start = unlist(getOptPathEl(opt.path, i)$x)
 #   capture.output(design <- max_EI(model$learner.model,
-#     lower=low, upper=upp, parinit=start)$par)
+#     lower = low, upper = upp, parinit = start)$par)
 #   as.data.frame(design)
 # }
 
