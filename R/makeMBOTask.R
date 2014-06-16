f5fbe16ba6430c3120986a656fc624d67f258dae
danielhorn <daniel.horn@tu-dortmund.de>
2014-06-16 15:11:06 +0200

    Delete makeMBOTask.R

--- R/makeMBOTask.R
+++
@@ -1,6 +0,0 @@
-# Generates MBO task.
-
-makeMBOTask = function(design, par.set, control) {
-  data = convertOptPathToDf(par.set, opt.path, control, impute.feats = TRUE)
-  makeRegrTask(target = control$y.name, data = data)
-}
