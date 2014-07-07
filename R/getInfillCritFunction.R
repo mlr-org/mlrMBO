5253a3222cc5e50a32ec26f92aee172b91a0366c
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-08 01:27:59 +0200

    clean up

--- R/getInfillCritFunction.R
+++ R/getInfillCritFunction.R
@@ -1,20 +1,21 @@
 # trivial dispatcher
 getInfillCritFunction = function(infill.crit) {
   switch(infill.crit,
-         mean = infillCritMeanResponse,
-         ei = infillCritEI,
-         aei = infillCritAEI,
-         lcb = infillCritLCB,
-         akg = infillCritAKG,
-         eqi = infillCritEQI,
-         mq  = infillCritMQ,
-         eipi  =  infillCritEIPI,
-         multiFid = infillCritMultiFid,
-         match.fun(infill.crit)
+    mean = infillCritMeanResponse,
+    ei = infillCritEI,
+    # aei = infillCritAEI,
+    lcb = infillCritLCB,
+    # akg = infillCritAKG,
+    # eqi = infillCritEQI,
+    # mq  = infillCritMQ,
+    # eipi  =  infillCritEIPI,
+    multiFid = infillCritMultiFid,
+    match.fun(infill.crit)
   )
 }
 
 
-getSupportedInfillCritFunctions = function() {
+#FIXME: export
+getInfillCrits = function() {
   c("mean", "ei", "aei", "lcb")
 }
