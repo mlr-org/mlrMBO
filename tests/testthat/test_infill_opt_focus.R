7ce7cf9df313e2565413fd89022ee58b9dcd854c
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-08 10:01:30 +0200

    clean up

--- tests/testthat/test_infill_opt_focus.R
+++ tests/testthat/test_infill_opt_focus.R
@@ -43,7 +43,7 @@ test_that("dependent params, but no focussing", {
   ctrl = setMBOControlInfill(ctrl, opt = "focussearch", opt.restarts = 1, opt.focussearch.maxit = 1,
     opt.focussearch.points = 500)
   or = mbo(f, ps, learner = learner, control = ctrl, show.info = FALSE)
-  expect_true(!is.na(as.data.frame(or$opt.path)[[".model.fail"]]))
+  expect_true(all(is.na(as.data.frame(or$opt.path)[[".model.fail"]])))
   expect_true(!is.na(or$y))
 })
 
