9bc16d43db4673b0843805be0120799169b6cd0d
Jakob Bossek <info@jakobbossek.de>
2014-07-10 13:18:41 +0200

    clean up

--- R/calculateGap.R
+++ R/calculateGap.R
@@ -1,8 +1,8 @@
 # calculates gap between best point and global optimum
 calculateGap = function(design, global.opt, control) {
-  best.y = if(control$minimize)
+  best.y = if (control$minimize)
     min(design[, control$y.name])
   else
     max(design[, control$y.name])
-  gap =  abs(best.y - global.opt)
+  gap = abs(best.y - global.opt)
 }
