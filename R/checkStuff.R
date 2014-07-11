f2dba5d69534733db0fbf81272516e1fe7a70adf
Jakob Bossek <info@jakobbossek.de>
2014-07-11 17:06:11 +0200

    clean up checkStuff

--- R/checkStuff.R
+++ R/checkStuff.R
@@ -18,7 +18,7 @@ checkStuff = function(fun, par.set, design, learner, control) {
 
   pids = getParamIds(par.set)
 
-  # check params + learner
+  # general parameter and learner checks
   if (any(sapply(par.set$pars, inherits, what = "LearnerParam")))
     stop("No parameter can be of class 'LearnerParam'! Use basic parameters instead to describe you region of interest!")
 
@@ -27,11 +27,11 @@ checkStuff = function(fun, par.set, design, learner, control) {
 
   if (hasDiscrete(par.set) && !hasProperties(learner, "factors"))
     stop("Provided learner does not support factor parameters.")
-  
+
   if (learner$type != "regr")
     stop("mbo requires regression learner!")
 
-  # general infill stuff. relavant for single obj and parego
+  # general infill stuff (relavant for single objective and parEGO)
   if (control$infill.crit %in% c("ei", "aei", "lcb") && learner$predict.type != "se") {
     stopf("For infill criterion '%s' predict.type of learner %s must be set to 'se'!%s",
       control$infill.crit, learner$id,
@@ -39,13 +39,13 @@ checkStuff = function(fun, par.set, design, learner, control) {
         "\nBut this learner does not seem to support prediction of standard errors! You could use the mlr wrapper makeBaggingWrapper to bootstrap the standard error estimator."))
   }
 
+  # compatibility of optimizers and parameters
   if (control$infill.opt %in% c("cmaes", "ea") && !isNumeric(par.set))
     stopf("Optimizer '%s' can only be applied to numeric, integer, numericvector, integervector parameters!", control$infill.opt)
 
-  # For now allow constant liar only in combinaton with Kriging
-  # (see https://github.com/berndbischl/mlrMBO/issues/12)
+  # For now allow constant liar only in combinaton with kriging
   if (control$multipoint.method == "cl" && learner$id != "regr.km")
-    stop("Constant liar can currently only be used with Kriging surrograte.")
+    stop("Constant liar can currently only be used with kriging.")
 
   #  single objective
   if (control$number.of.targets == 1L) {
@@ -65,14 +65,10 @@ checkStuff = function(fun, par.set, design, learner, control) {
     }
   }
 
-  # for EI we need mu + sd
-  #  if (control$infill.opt == "EI" &&
-  #          !(class(learner) %in% c("regr.km", "regr.kmforrester")))
-  #      stop("Expected improvement can currently only be used with learner 'regr.km' and 'regr.kmforrester'!")
   # if (!(inherits(learner, "regr.randomForest") || inherits(learner, "regr.rpart")) && hasRequires(par.set))
   #   stop("Parameter sets with dependent parameters currently require the learner to be a randomForest or rpart.")
 
-  ##### multifid #####
+  # multifidelity stuff
   if (control$multifid) {
     if (control$multifid.param %nin% pids)
       stopf("Fidelity param '%s' must be in par.set!", control$multifid.param)
