0fa2e3f7d34cb3fbe644efcb5af4662408d1f799
Jakob Bossek <info@jakobbossek.de>
2014-07-11 09:56:10 +0200

    fix 1d discrete function example

--- inst/examples/ex_1d_3.R
+++ inst/examples/ex_1d_3.R
@@ -19,24 +19,22 @@ objfun = function(x) {
   if (x$foo == "a") {
     return(5 + rnorm(1))
   } else if (x$foo == "b") {
-    return(4 + rnorm(1, sd=0.5))
+    return(4 + rnorm(1, sd = 0.5))
   } else {
-    return(3 + rnorm(1, sd=1))
+    return(3 + rnorm(1, sd = 1))
   }
 }
 
-ps = makeParamSet(
-  makeDiscreteParam("foo", values = c("a", "b", "c"))
-)
+par.set = makeParamSet(makeDiscreteParam("foo", values = letters[1:3]))
 
-ctrl = makeMBOControl(init.design.points = 20, iters = 5, infill.crit = "ei",
-  infill.opt.focussearch.points = 100, noisy = TRUE)
+ctrl = makeMBOControl(init.design.points = 20, iters = 5, noisy = TRUE)
+ctrl = setMBOControlInfill(ctrl, crit = "ei", opt.focussearch.points = 100)
 
 lrn = makeLearner("regr.randomForest", predict.type = "se")
 
-run = exampleRun(objfun, par.set = ps, learner = lrn, control = ctrl, points.per.dim = 50)
+run = exampleRun(objfun, par.set = par.set, learner = lrn, control = ctrl, points.per.dim = 50)
 
 print(run)
 
-autoplot(run, pause = TRUE, densregion = TRUE)
+res = autoplot(run, pause = FALSE, densregion = TRUE)
 
