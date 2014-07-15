035b1d0c0d8e22d90209e25288d95c1ad2770284
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-15 17:20:24 +0200

    more tests for different learners

--- tests/testthat/test_different_learners.R
+++ tests/testthat/test_different_learners.R
@@ -1,22 +1,14 @@
 context("different learner")
 
 test_that("mbo works with different learners", {
-  
-  ## This function works, the other does not ..???
-  #f = function(x) if (x$x3 == "a") x$x1^2 + x$x2^2 else 5
-  #ps = makeParamSet(
-  #  makeNumericParam("x1", lower = -2, upper = 1),
-  #  makeNumericParam("x2", lower = -1, upper = 2),
-  #  makeDiscreteParam("x3", values = c("a", "b"))
-  #)
-  
+
   f = function(x)
     ifelse(x$disc1 == "a", x$num1 * 2 - 1, 1 - x$num1)
   ps = makeParamSet(
     makeDiscreteParam("disc1", values = c("a", "b")),
     makeNumericParam("num1", lower = 0, upper = 1)
   )
-  
+
   ctrl = makeMBOControl(iters = 2, init.design.points = 10)
   ctrl = setMBOControlInfill(ctrl, opt.focussearch.points = 100)
 
@@ -26,4 +18,25 @@ test_that("mbo works with different learners", {
   mbo(f, ps, learner = makeLearner("regr.kknn"), control = ctrl)
   mbo(f, ps, learner = makeLearner("regr.ksvm"), control = ctrl)
   mbo(f, ps, learner = makeLearner("regr.earth"), control = ctrl)
+
+  ps = makeParamSet(
+    makeNumericVectorParam("x", len = 2, lower = 0, upper = 1),
+    makeDiscreteParam("z", values = 1:5)
+  )
+  f = function(x) sum(x$x) + as.numeric(x$z)
+  # check with larger initial design so all factor levels are there
+  ctrl = makeMBOControl(iters = 2, init.design.points = 30)
+  ctrl = setMBOControlInfill(ctrl, opt.focussearch.points = 100)
+
+  testit = function(lrn) {
+    lrn = makeLearner(lrn)
+    lrn = makeBaggingWrapper(lrn, bw.iters = 10L)
+    lrn = setPredictType(lrn, "se")
+    mbo(f, ps,learner = lrn, control = ctrl)
+  }
+
+  testit("regr.lm")
+  testit("regr.blackboost")
+  testit("regr.nnet")
 })
+
