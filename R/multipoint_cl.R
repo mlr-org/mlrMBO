b68f95b36720c044078b79a15c9fd908b38548fe
Jakob Bossek <info@jakobbossek.de>
2014-07-15 10:20:03 +0200

    repair constant liar

--- R/multipoint_cl.R
+++ R/multipoint_cl.R
@@ -9,26 +9,25 @@ multipointInfillOptCL = function(model, control, par.set, opt.path, design, ...)
   control2$propose.points = 1L
   control2$infill.crit = "ei"
   newdes = data.frame()
-  op2 = opt.path
+  opt.path2 = opt.path
   #FIXME this is bad code
-  op2$env = new.env()
-  op2$env$path = opt.path$env$path
-  op2$env$dob = opt.path$env$dob
-  op2$env$eol = opt.path$env$eol
+  opt.path2$env = new.env()
+  opt.path2$env$path = opt.path$env$path
+  opt.path2$env$dob = opt.path$env$dob
+  opt.path2$env$eol = opt.path$env$eol
   lie = min(getOptPathY(opt.path, y.name))
   dob = max(getOptPathDOB(opt.path)) + 1
-  prop.points.crit.values = data.frame()
+  crit.vals = data.frame()
   while (nrow(newdes) < control$propose.points) {
-    newdes1 = proposePoints(model, par.set, control2, op2)
+    newdes1 = proposePoints(model, par.set, control2, opt.path2)
     x = dfRowToList(newdes1$prop.points, par.set, 1)
-    addOptPathEl(op2, x = x, y = lie, dob = dob)
+    addOptPathEl(opt.path2, x = x, y = lie, dob = dob)
     newdes = rbind(newdes, newdes1$prop.points)
-    prop.points.crit.values=c(prop.points.crit.values, newdes1$prop.points.crit.values)
+    crit.vals = c(crit.vals, newdes1$crit.vals)
     
-    # update model
-    rt = makeMBOSingleObjTask(as.data.frame(op2, discretes.as.factor = TRUE),
-      par.set, control = control)
+    # update model  
+    rt = makeMBOSingleObjTask(par.set, as.data.frame(opt.path2, discretes.as.factor = TRUE), control = control)
     model = train(learner, rt)
   }
-  return(list(prop.points = newdes, prop.points.crit.values = prop.points.crit.values))
+  return(list(prop.points = newdes, crit.vals = crit.vals))
 }
