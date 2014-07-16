81a32c3cfaaa9104ce8bf45830d5c25e53e2fa1f
Daniel Horn <daniel.horn@tu-dortmund.de>
2014-07-16 14:46:15 +0200

    improvements for the visualization

--- bench/mco/paregoScalarize.R
+++ bench/mco/paregoScalarize.R
@@ -1,7 +1,7 @@
 source("bench/mco/WFG.R")
 
-makePlot = function (rho, lambda) {
-  sequence = seq(0, 1, length.out = 100)
+makePlot = function (rho, lambda, scale = 5.2) {
+  sequence = seq(0, scale, length.out = 100)
   y = expand.grid(sequence, sequence)
   weighted.y = t(t(y) * lambda)
   
@@ -11,17 +11,18 @@ makePlot = function (rho, lambda) {
   apply(y.star, 1, which.min)
   
   
-  lines(lambda[2] * seq(0, 1, 0.01), lambda[1] * seq(0, 1, 0.01), col = "red", lwd = 2)
+  lines(lambda[2] * seq(0, scale, 0.01), lambda[1] * seq(0, scale, 0.01), col = "red", lwd = 2)
   f = function(x, lambda, rho, const) {
     y1 = (const - lambda[1] * x * (1 + rho)) / (rho * lambda[2])
     y2 = (const - lambda[1] * x * rho) / ((1 + rho) * lambda[2])
     pmin(y1, y2)
   }
-  x = 0.75 * lambda[2:1]
+  x = scale * 0.85 * lambda[2:1]
   const = max(lambda * x) + rho * lambda * x
   lines(sequence, f(sequence, lambda, rho, const), col = "blue", lwd = 2)
   
 }
 
 
-makePlot(0.05, c(0.05, 0.95))
+makePlot(0.05, c(0.9, 0.1))
+points(t(wfg4pf(2, 100)), type = "l", lwd = 2)
