08628352ccf8653fc094e4b60dd4018746e2f532
Jakob Bossek <info@jakobbossek.de>
2014-08-14 10:26:00 +0200

    overwork trafo functions

--- R/makeMBOSingleObjTask.R
+++ R/makeMBOSingleObjTask.R
@@ -19,10 +19,8 @@ makeMBOSingleObjTask = function(par.set, opt.path, control) {
   trafo.y.fun = control$trafo.y.fun
   if (!is.null(trafo.y.fun)) {
   	y.name = control$y.name
-  	if (any(data[[y.name]] < 0)) {
-  		stopf("Transformation '%s' required all inputs to be positive, but negative values occured.")
-  	}
-  	data[[y.name]] = trafo.y.fun(data[[y.name]])
+    # We stop the process if negative values occur
+  	data[[y.name]] = trafo.y.fun(data[[y.name]], handle.violations = "error")
   }
 
   makeRegrTask(target = control$y.name, data = data)
--- R/makeMBOTrafoFunction.R
+++ R/makeMBOTrafoFunction.R
@@ -8,6 +8,8 @@
 #' @seealso \link{trafos}
 #' @export
 makeMBOTrafoFunction = function(name, fun) {
+  assertString(name, na.ok = FALSE)
+  assertFunction(fun)
   fun = addClasses(fun, "MBOTrafoFunction")
   fun = setAttribute(fun, "name", name)
   return(fun)
@@ -16,12 +18,13 @@ makeMBOTrafoFunction = function(name, fun) {
 #' Transformation methods.
 #'
 #' \itemize{
-#' \item{\bold{logTrafo}}{\cr Natural logarithm.}
-#' \item{\bold{log10Trafo}}{\cr Logartihm to base 10.}
-#' \item{\bold{sqrtTrafo}}{\cr Square root.}
+#'   \item{\bold{logTrafo}}{\cr Natural logarithm.}
+#'   \item{\bold{sqrtTrafo}}{\cr Square root.}
 #' }
 #' If negative values occur and the trafo function can handle only positive values,
-#' a shift of the form x - min(x) + 1 is performed prior to the transformation.
+#' a shift of the form x - min(x) + 1 is performed prior to the transformation if the
+#' argument \code{handle.violations} is set to \dQuote{warn} which is the default
+#' value. 
 #' @format None
 #' @name trafos
 #' @rdname trafos
@@ -29,30 +32,16 @@ NULL
 
 #' @export
 #' @rdname trafos
-trafoLog = function() {
+#' @param base [\code{numeric(1)}]\cr
+#'   The base with respect to which logarithms are computed.
+#'   Default is \code{10}.
+trafoLog = function(base = 10) {
+  assertNumber(base, na.ok = FALSE, lower = 2L)
   makeMBOTrafoFunction(
     name = "log",
-    fun = function(x) {
-      if (any(x < 0)) {
-        warning("Negative function values. Shifting function to apply logarithm.")
-        x = x - min(x) + 1
-      }
-      return(log(x))
-    }
-  )
-}
-
-#' @export
-#' @rdname trafos
-trafoLog10 = function() {
-  makeMBOTrafoFunction(
-    name = "log10",
-    fun = function(x) {
-      if (any(x < 0)) {
-        warning("Negative function values. Shifting function to apply logarithm.")
-        x = x - min(x) + 1
-      }
-      return(log10(x))
+    fun = function(x, handle.violations = "warn") {
+      x = checkAndRepairNegativeValues(x, handle.violations)
+      log(x, base = base)
     }
   )
 }
@@ -62,8 +51,20 @@ trafoLog10 = function() {
 trafoSqrt = function() {
   makeMBOTrafoFunction(
     name = "sqrt",
-    fun = function(x) {
+    fun = function(x, handle.violations = "warn") {
+      x = checkAndRepairNegativeValues(x, handle.violations)
       sqrt(x)
     }
   )
+}
+
+checkAndRepairNegativeValues = function(x, handle.violations) {
+  assertChoice(handle.violations, c("warn", "error"))
+  if (any(x < 0)) {
+    if (handle.violations == "error")
+      stopf("Negative function values occured during transformation.")
+    warning("Negative function values. Shifting function to apply logarithm.")
+    return(x - min(x) + 1)
+  }
+  return(x)
 }
