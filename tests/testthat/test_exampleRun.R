5cc427aaac27179cb3e46030a369d0a0414e738e
Jakob Bossek <info@jakobbossek.de>
2014-08-14 09:30:11 +0200

    reenable exampleRun/autoplot test

--- tests/testthat/test_exampleRun.R
+++ tests/testthat/test_exampleRun.R
@@ -1,24 +1,40 @@
 context("exampleRun")
 
-#FIXME renable
+test_that("exampleRun", {
+	library(ggplot2)
+	
+	n.iters = 2L
 
-# test_that("exampleRun", {
-#
-#   doRun = function(f, par.set, predict.type, crit) {
-#     learner = makeLearner("regr.randomForest", ntree=3, predict.type=predict.type)
-#     ctrl = makeMBOControl(init.design.points=20, iters=2, infill.crit=crit,
-#       infill.opt="focussearch", infill.opt.focussearch.points=10)
-#     run = exampleRun(makeMBOFunction(f), par.set, learner, ctrl)
-#     plot(run)
-#   }
-#
-#   f = function(x) sum(x*x)
-#   ps = makeParamSet(
-#     makeNumericParam("x", lower=-2, upper=2)
-#   )
-#   # with se
-#   doRun(f, ps, "response", "mean")
-#   # with se
-#   doRun(f, ps, "se", "ei")
-#     # without se
-# })
+	doRun = function(obj.fn, par.set, predict.type, crit) {
+		learner = makeLearner("regr.km", predict.type = predict.type)
+		control = makeMBOControl(init.design.points = 10, iters = n.iters)
+		control = setMBOControlInfill(control, crit = crit, opt = "focussearch", opt.focussearch.points = 10)
+		run = exampleRun(makeMBOFunction(obj.fn), global.opt = 0L, par.set, learner, control = control)
+		return(autoplot(run, pause = FALSE, show.info = FALSE))
+	}
+
+	obj.fn = function(x) sum(x*x)
+	par.set = makeParamSet(
+		makeNumericParam("x", lower = -2, upper = 2)
+	)
+
+	checkPlotList = function(plot.list) {
+		expect_is(plot.list, "list")
+		expect_equal(length(plot.list), n.iters)
+		lapply(plot.list, function(pl.sublist) {
+			expect_is(pl.sublist, "list")
+			lapply(pl.sublist, function(pl) {
+				expect_is(pl, "gg")
+				expect_is(pl, "ggplot")
+			})
+		})
+	}
+
+	# without se
+	plot.list = doRun(obj.fn, par.set, "response", "mean")
+	checkPlotList(plot.list)
+
+	# with se
+	plot.list = doRun(obj.fn, par.set, "se", "ei")
+	checkPlotList(plot.list)
+})
