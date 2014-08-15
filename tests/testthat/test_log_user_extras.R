bf7b52e59e58d34822c0a0d482cbcdd8b4498b91
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-15 15:00:32 +0200

    clean up

--- tests/testthat/test_log_user_extras.R
+++ tests/testthat/test_log_user_extras.R
@@ -1,30 +1,30 @@
 context("test user extras")
 
 test_that("user can log extra stuff to optimization path via the 'extras' attribute of the target function values", {
-	
-	extras.names = c("extra1", "extra2")
 
-	obj.fn = function(x) {
-		res = sum(x$x^2)
-		# here we append additional stuff to be logged in opt path
-		res = setAttribute(res, "extras", list(extra1 = runif(1), extra2 = letters[sample(1:10, 1)]))
-		return(res)
-	}
+  extras.names = c("extra1", "extra2")
 
-	par.set = makeParamSet(
-		makeNumericParam("x", lower = -2, upper = 2)
-	)
+  obj.fn = function(x) {
+    res = sum(x$x^2)
+    # here we append additional stuff to be logged in opt path
+    res = setAttribute(res, "extras", list(extra1 = runif(1), extra2 = letters[sample(1:10, 1)]))
+    return(res)
+  }
 
-	learner = makeLearner("regr.km", predict.type = "se")
-	control = makeMBOControl(init.design.points = 5L, iters = 2L)
-	control = setMBOControlInfill(control, crit = "ei", opt = "focussearch")
+  par.set = makeParamSet(
+    makeNumericParam("x", lower = -2, upper = 2)
+    )
 
-	# run mbo and convert opt path to data frame
-	result = mbo(obj.fn, par.set, learner = learner, control = control, show.info = FALSE)
-	opt.path = as.data.frame(result$opt.path)
+  learner = makeLearner("regr.km", predict.type = "se")
+  control = makeMBOControl(init.design.points = 5L, iters = 2L)
+  control = setMBOControlInfill(control, crit = "ei", opt = "focussearch")
 
-	# check that user extras are in fact logged
-	expect_true(all(extras.names %in% names(opt.path)))
-	expect_true(all(is.numeric(opt.path[["extra1"]])))
-	expect_true(all(opt.path[["extra2"]] %in% letters[1:10]))
+  # run mbo and convert opt path to data frame
+  result = mbo(obj.fn, par.set, learner = learner, control = control, show.info = FALSE)
+  opt.path = as.data.frame(result$opt.path)
+
+  # check that user extras are in fact logged
+  expect_true(all(extras.names %in% names(opt.path)))
+  expect_true(all(is.numeric(opt.path[["extra1"]])))
+  expect_true(all(opt.path[["extra2"]] %in% letters[1:10]))
 })
