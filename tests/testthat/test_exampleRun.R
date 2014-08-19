75c518aac49727681960d994d50c1fa17aadb7db
Jakob Bossek <info@jakobbossek.de>
2014-08-19 13:52:29 +0200

    enable test for autoplot of mixed discrete/numeric functions

--- tests/testthat/test_exampleRun.R
+++ tests/testthat/test_exampleRun.R
@@ -2,14 +2,17 @@ context("exampleRun")
 
 test_that("exampleRun", {
 	library(ggplot2)
-	
+	library(soobench)
+
 	n.iters = 2L
 
-	doRun = function(obj.fn, par.set, predict.type, crit) {
-		learner = makeLearner("regr.km", predict.type = predict.type)
+	doRun = function(obj.fn, par.set, predict.type, crit, learner = "regr.km", has.simple.signature = TRUE) {
+		learner = makeLearner(learner, predict.type = predict.type)
 		control = makeMBOControl(init.design.points = 10, iters = n.iters)
 		control = setMBOControlInfill(control, crit = crit, opt = "focussearch", opt.focussearch.points = 10)
-		run = exampleRun(makeMBOFunction(obj.fn), global.opt = 0L, par.set, learner, control = control)
+		if (has.simple.signature)
+			obj.fn = makeMBOFunction(obj.fn)
+		run = exampleRun(obj.fn, global.opt = 0L, par.set, learner, control = control)
 		return(autoplot(run, pause = FALSE, show.info = FALSE))
 	}
 
@@ -36,5 +39,22 @@ test_that("exampleRun", {
 
 	# with se
 	plot.list = doRun(obj.fn, par.set, "se", "ei")
+	checkPlotList(plot.list)
+
+	obj.fn = function(x) {
+	  	if (x$foo == "a")
+	    	sum(x$x^2)
+	  	else if (x$foo == "b")
+	    	sum(x$x^2) + 10
+	  	else
+	    	sum(x$x^2) - 10
+	}
+
+	par.set = makeParamSet(
+	  	makeDiscreteParam("foo", values = letters[1:3]),
+	  	makeNumericVectorParam("x", len = 1, lower = -2, upper = 3)
+	)
+
+	plot.list = doRun(obj.fn, par.set, "se", "ei", "regr.randomForest", has.simple.signature = FALSE)
 	checkPlotList(plot.list)
 })
