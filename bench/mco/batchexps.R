a6eec77b89d9f292842e1198055fa03ef2a425ce
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-09-09 17:18:36 +0200

    clean up and extend bench/mco

--- bench/mco/batchexps.R
+++ bench/mco/batchexps.R
@@ -25,53 +25,42 @@ reg = makeExperimentRegistry("mco_bench", packages = c(
 addMyProblem = function(id, objective, lower, upper) {
   addProblem(reg, id = id,, static = list(
       objective = objective,
-      par.set = makeNumericParamSet("x", len = 5L, lower = lower, upper = upper),
-      ny = 2L,
-      ref = c(11, 11)
+      par.set = makeNumericParamSet("x", len = DIM_X, lower = lower, upper = upper),
+      ny = DIM_Y,
+      ref = REFERENCE_POINT,
+      makeResult = function(set, front, ref) {
+        hv = dominated_hypervolume(t(front), ref = ref)
+        cd = crowding_distance(t(front))
+        r2 = unary_r2_indicator(t(front), weights = WEIGHTS)
+        list(pareto.set = set, pareto.front = front, hv = hv, cd = cd, r2 = r2)
+      }
   ))
 }
 
 # test functions
-for (i in 1:6) {
+for (i in 1:1) {
   fname = sprintf("dtlz%i", i)
   addMyProblem(fname, get(fname), lower = 0, upper = 1)
 }
-for (i in c(1:5, 7:9)) {
-  fname = sprintf("lz%i", i)
-  addMyProblem(fname, get(fname), lower = 0, upper = 1)
-}
-for (i in 4:7) {
-  fname = sprintf("uf%i", i)
-  addMyProblem(fname, get(fname), lower = 0, upper = 1)
-}
-for (i in 1:9) {
-  fname = sprintf("wfg%i", i)
-  addMyProblem(fname, get(fname), lower = 0, upper = 1)
-}
-for (i in c(1:4, 6)) {
-  fname = sprintf("zdt%i", i)
-  addMyProblem(fname, get(fname), lower = 0, upper = 1)
-}
+# for (i in c(1:5, 7:9)) {
+#   fname = sprintf("lz%i", i)
+#   addMyProblem(fname, get(fname), lower = 0, upper = 1)
+# }
+# for (i in 4:7) {
+#   fname = sprintf("uf%i", i)
+#   addMyProblem(fname, get(fname), lower = 0, upper = 1)
+# }
+# for (i in 1:9) {
+#   fname = sprintf("wfg%i", i)
+#   addMyProblem(fname, get(fname), lower = 0, upper = 1)
+# }
+# for (i in c(1:4, 6)) {
+#   fname = sprintf("zdt%i", i)
+#   addMyProblem(fname, get(fname), lower = 0, upper = 1)
+# }
 
 
-
-addAlgorithm(reg, "nsga2", fun = function(static, generations) {
-  par.set = static$par.set
-  names.x = getParamIds(par.set, repeated = TRUE, with.nr = TRUE)
-
-  res = nsga2(static$objective, idim = getParamNr(par.set, devectorize = TRUE),
-    odim = static$ny, lower.bounds = getLower(par.set), upper.bounds = getUpper(par.set),
-    popsize = POPSIZE, generations = generations)
-  pareto.set = setColNames(as.data.frame(subset(res$par, res$pareto.optimal, drop = FALSE)),
-    names.x)
-  pareto.front = subset(res$value, res$pareto.optimal, drop = FALSE)
-  hv = dominated_hypervolume(t(pareto.front), ref = static$ref)
-  cd = crowding_distance(t(pareto.front))
-  r2 = unary_r2_indicator(t(pareto.front), weights = WEIGHTS)
-  list(pareto.set = pareto.set, pareto.front = pareto.front, hv = hv, cd = cd, r2 = r2)
-})
-
-addAlgorithm(reg, "parego", fun = function(static, prop.points) {
+runMBO = function(static, method, crit, opt, prop.points) {
   par.set = static$par.set
   names.x = getParamIds(par.set, repeated = TRUE, with.nr = TRUE)
 
@@ -81,32 +70,67 @@ addAlgorithm(reg, "parego", fun = function(static, prop.points) {
   ctrl = makeMBOControl(number.of.targets = static$ny, init.design.points = INIT_DESIGN_POINTS,
     iters = iters, propose.points = prop.points,
     save.on.disk.at = integer(0L))
-  ctrl = setMBOControlInfill(ctrl, crit = "ei", opt.focussearch.points = 10000L,
-    opt.restarts = 3L, opt.focussearch.maxit = 3L)
-  ctrl = setMBOControlMultiCrit(ctrl)
+  ctrl = setMBOControlInfill(ctrl, crit = crit, opt = opt,
+    opt.focussearch.points = FOCUSSEARCH_POINTS,
+    opt.restarts = FOCUSSEARCH_RESTARTS,
+    opt.focussearch.maxit = FOCUSSEARCH_MAXIT,
+    opt.nsga2.generations = MSPOT_NSGA2_GENERATIONS,
+    opt.nsga2.popsize = MSPOT_NSGA2_POPSIZE
+  )
+  ctrl = setMBOControlMultiCrit(ctrl, method = method)
 
   res = mbo(makeMBOFunction(static$objective), static$par.set,
     learner = learner, control = ctrl, show.info = TRUE)
-  hv = dominated_hypervolume(t(res$pareto.front), ref = static$ref)
-  cd = crowding_distance(t(res$pareto.front))
-  r2 = unary_r2_indicator(t(res$pareto.front), weights = WEIGHTS)
-  list(paretoset = res$pareto.set, pareto.front = res$pareto.front, hv = hv, cd = cd, r2 = r2)
+  static$makeResult(res$pareto.set, res$pareto.front, static$ref)
+}
+
+
+addAlgorithm(reg, "nsga2", fun = function(static, generations) {
+  par.set = static$par.set
+  names.x = getParamIds(par.set, repeated = TRUE, with.nr = TRUE)
+
+  res = nsga2(static$objective, idim = getParamNr(par.set, devectorize = TRUE),
+    odim = static$ny, lower.bounds = getLower(par.set), upper.bounds = getUpper(par.set),
+    popsize = BASELINE_NSGA2_POPSIZE, generations = generations)
+  pareto.set = setColNames(as.data.frame(subset(res$par, res$pareto.optimal, drop = FALSE)),
+    names.x)
+  pareto.front = subset(res$value, res$pareto.optimal, drop = FALSE)
+  static$makeResult(pareto.set, pareto.front, static$ref)
+})
+
+addAlgorithm(reg, "parego", fun = function(static, prop.points) {
+  runMBO(static, "parego", "ei", "focussearch", prop.points)
+})
+
+addAlgorithm(reg, "sms", fun = function(static, prop.points) {
+  runMBO(static, "sms", "sms", "focussearch", prop.points)
+})
+
+addAlgorithm(reg, "mspot", fun = function(static, prop.points) {
+  runMBO(static, "mspot", "ei", "nsga2", prop.points)
 })
 
 
 des1 = makeDesign("nsga2", exhaustive = list(
-  generations = c(GENERATIONS1, GENERATIONS2)
+  generations = c(BASELINE_NSGA2_GENERATIONS1, BASELINE_NSGA2_GENERATIONS2)
+))
+des2 = makeDesign("parego", exhaustive = list(
+  prop.points = c(1L, PARALLEL_PROP_POINTS)
+))
+des3 = makeDesign("sms", exhaustive = list(
+  prop.points = c(1L)
+))
+des3 = makeDesign("mspot", exhaustive = list(
+  prop.points = c(1L)
 ))
 
-des2 = makeDesign("parego", exhaustive = list(
-  prop.points = c(PROP_POINTS1, PROP_POINTS2)
-))
 
 addExperiments(reg, algo.design = des1, repls = REPLS)
 addExperiments(reg, algo.design = des2, repls = REPLS)
+addExperiments(reg, algo.design = des3, repls = REPLS)
 
-j = findExperiments(reg, algo.pattern = "pare")
-testJob(reg, j[1], external = F)
+# j = findExperiments(reg, algo.pattern = "pare")
+# testJob(reg, j[1], external = F)
 # submitJobs(reg, ids = chunk(getJobIds(reg), n.chunks = 33, shuffle = TRUE),
   # resources=list(walltime=8*3600, memory=2*1024),
   # wait=function(retries) 1, max.retries=10)
