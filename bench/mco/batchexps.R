eb92799d06c4685d32ac73c1e66e254232595ede
DanielHorn <daniel.horn@tu-dortmund.de>
2014-09-21 14:24:46 +0200

    cleanup

--- bench/mco/batchexps.R
+++ bench/mco/batchexps.R
@@ -122,6 +122,6 @@ addExperiments(reg, algo.design = des3, repls = REPLS)
 addExperiments(reg, algo.design = des4, repls = REPLS)
 
 batchExport(reg, runMBO = runMBO)
- submitJobs(reg, ids = chunk(getJobIds(reg), n.chunks = 35L, shuffle = TRUE),
+ submitJobs(reg, ids = chunk(getJobIds(reg), n.chunks = 250L, shuffle = TRUE),
    resources=list(walltime=1*3600, memory=2*1024),
    wait=function(retries) 1, max.retries=10)
