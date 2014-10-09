3c7244165f1961ae97afa05ae5cb2f4bddd67b6c
nbauer2 <bauer@bauer2.fb05.statistik.uni-dortmund.de>
2014-10-09 12:53:28 +0200

    add two noisy infillcrits (aei and eqi)

--- R/getSupportedInfillCritFunctions.R
+++ R/getSupportedInfillCritFunctions.R
@@ -3,6 +3,6 @@
 #' @return [\code{character}]
 #' @export
 getSupportedInfillCritFunctions = function() {
-  c("mean", "se", "ei", "aei", "lcb", "dib", "multiFid")
+  c("mean", "se", "ei", "aei", "eqi","lcb", "dib", "multiFid")
 }
 
