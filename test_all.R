748b8f1f5224e5e95882310c16bd5d4a60fa7a0b
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-10 16:04:33 +0200

    clean up

--- test_all.R
+++ test_all.R
@@ -4,12 +4,8 @@ library(devtools)
 library(mlr)
 library(soobench)
 
-if (interactive()) {
-  load_all(".", reset=TRUE)
-} else {
-  library(mlrMBO)
-}
+load_all(".", reset = TRUE)
 
-configureMlr(show.learner.output=FALSE)
+configureMlr(show.learner.output = FALSE)
 test_dir("tests/testthat")
 
