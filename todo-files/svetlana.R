4650d13652378d3240a3f0a9f010a15dc30e5d5a
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-29 09:46:31 +0100

    todo files plots for optpath

--- todo-files/svetlana.R
+++ todo-files/svetlana.R
@@ -20,17 +20,17 @@ z =load2("~/Desktop/saveopt.RData")
 print(plotOptPath(z$opt.path, z$control))
 
 
-ps = z$par.set
-opdf = as.data.frame(z$opt.path)
-xy = as.data.frame(z$opt.path, include.rest = FALSE)
-task = makeRegrTask(data = xy, target = "y")
+# ps = z$par.set
+# opdf = as.data.frame(z$opt.path)
+# xy = as.data.frame(z$opt.path, include.rest = FALSE)
+# task = makeRegrTask(data = xy, target = "y")
 
-z$learner$fix.factors = FALSE
-mod = train(z$learner, task)
-des = generateRandomDesign(1000L, z$opt.path$par.set)
-pred = predict(mod, newdata = des)$data$response
-idx = pred < 0.43
-good = des[idx, ]
-r= sapply(good, range)
-print(r)
+# z$learner$fix.factors = FALSE
+# mod = train(z$learner, task)
+# des = generateRandomDesign(1000L, z$opt.path$par.set)
+# pred = predict(mod, newdata = des)$data$response
+# idx = pred < 0.43
+# good = des[idx, ]
+# r= sapply(good, range)
+# print(r)
 
