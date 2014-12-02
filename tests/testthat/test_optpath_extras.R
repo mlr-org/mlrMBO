9d889e5464f031207052d7ca3fe4769918c8e3a7
Jakob Bossek <info@jakobbossek.de>
2014-12-02 12:06:38 +0100

    rename test concerning extra stuff logged to optpath, added checks for train.time and exec.time

--- tests/testthat/test_optpath_extras.R
+++ tests/testthat/test_optpath_extras.R
@@ -1,9 +1,8 @@
-context("test user extras")
+context("test optpath extras")
 
-test_that("user can log extra stuff to optimization path via the 'extras' attribute of the target function values", {
+test_that("extras are logged a expected", {
 
-  extras.names = c("extra1", "extra2")
-
+  # Our test function stores additional stuff here via attributes
   obj.fn = function(x) {
     res = sum(x$x^2)
     # here we append additional stuff to be logged in opt path
@@ -13,7 +12,7 @@ test_that("user can log extra stuff to optimization path via the 'extras' attrib
 
   par.set = makeParamSet(
     makeNumericParam("x", lower = -2, upper = 2)
-    )
+  )
 
   learner = makeLearner("regr.km", predict.type = "se")
   control = makeMBOControl(init.design.points = 5L, iters = 2L)
@@ -21,10 +20,17 @@ test_that("user can log extra stuff to optimization path via the 'extras' attrib
 
   # run mbo and convert opt path to data frame
   result = mbo(obj.fn, par.set, learner = learner, control = control)
+
+  extras.names = c("extra1", "extra2")
+
   opt.path = as.data.frame(result$opt.path)
 
-  # check that user extras are in fact logged
+  # check whether user extras are in fact logged
   expect_true(all(extras.names %in% names(opt.path)))
   expect_true(all(is.numeric(opt.path[["extra1"]])))
   expect_true(all(opt.path[["extra2"]] %in% letters[1:10]))
+
+  # check whether times (model fitting, execution, ...) are logged
+  expect_true(all(is.numeric(opt.path[["exec.time"]])))
+  expect_true(all(is.numeric(opt.path[["train.time"]])))
 })
