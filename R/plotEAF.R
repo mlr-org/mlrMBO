3607fbf481a94a793e93a9c7888f1d46f20857a0
Jakob Bossek <info@jakobbossek.de>
2014-12-02 16:51:16 +0100

    reviewed, exported and tested  plotEAF

---
+++ R/plotEAF.R
@@ -0,0 +1,84 @@
+#' @title Plots attainment functions for data stored in multiple OptPaths.
+#'
+#' @description
+#' Can be used to plot OptPaths where information for bi-objective
+#' evaluation was logged for repeated runs of different algorithmic runs.
+#' Pretty directly calls \code{\link[eaf]{eafplot}}.
+#'
+#' @param opt.paths [\code{list}]\cr
+#'   List of list of \code{OptPath} objects. First index is the algorithm / major variation
+#'   in the experiment, second index is the index of the replicated run.
+#' @param xlim [\code{numeric(2)}]\cr
+#'   The x limits (x1, x2) of the plot.
+#' @param ylim [\code{numeric(2)}]\cr
+#'   The y limits (y1, y2) of the plot.
+#' @param ... [any]\cr
+#'   Passed on to \code{\link[eaf]{eafplot}}.
+#' @note
+#'   We changed the defaults of \code{\link[eaf]{eafplot}} in the following way:
+#'   The axis are labeled by \code{y.names}. But this can again be overwritten by the user.
+#' @return [\code{data.frame}]
+#'   Invisibly returns the data passed to \code{\link[eaf]{eafplot}}.
+#' @export
+plotEAF = function(opt.paths, xlim = NULL, ylim = NULL, ...) {
+  requirePackages("eaf", why = "plotEAF")
+
+  # we need a list of lists with optimization pathes 
+  assertList(opt.paths, min.len = 1L, types = "list", names = "unique")
+  algos = names(opt.paths)
+  y.names = NULL
+  minimize = NULL
+  data = data.frame()
+  for (i in seq_along(algos)) {
+    # extract opt path list for current algorithm
+    a = algos[i]
+    runs = opt.paths[[i]]
+    assertList(runs, types = "OptPath", min.len = 1L)
+
+    # combine all fronts for this algo + add algo / repl + do some sanity checks
+    fronts = lapply(seq_along(runs), function(j) {
+      run = runs[[j]]
+      f = as.data.frame(getOptPathParetoFront(run))
+      cns = colnames(f)
+      if (length(cns) != 2L) {
+        stop("Must always have 2 objectives in opt path. But found: %i", length(cns))
+      }
+      if (i == 1L && j == 1L) {
+        y.names <<- cns
+        minimize <<- run$minimize
+      }
+      if (!all(y.names == cns)) {
+        stop("Must always have the same 2 objectives in opt path: %s (first ones taken). But found here: %s",
+          collapse(y.names), collapse(cns))
+      }
+      if (!all(minimize == run$minimize)) {
+        stop("Must always have the same 'minimize' settings for objectives in opt path: %s (first one taken). 
+          But found here: %s", collapse(minimize), collapse(run$minimize))
+      }
+      # add column for algorithm and replication
+      cbind(f, .algo = a, .repl = j)
+    })
+    fronts = do.call(rbind, fronts)
+    data = rbind(data, fronts)
+  }
+  yn1 = y.names[1L]
+  yn2 = y.names[2L]
+  # build parameter list for eafplot function
+  f = as.formula(sprintf("%s + %s ~ .repl", yn1, yn2))
+  defaults = list(
+    xlab = yn1, ylab = yn2,
+    percentiles = 50
+  )
+  args = list(...)
+  args = insert(defaults, args)
+  args$x = f
+  args$data = data
+  args$groups = quote(.algo)
+  args$maximise = !minimize
+  args$xlim = xlim
+  args$ylim = ylim
+  args$col = c("darkgrey", "darkgrey", "darkgrey", "black", "black", "black")
+  args$lty = c("solid", "dashed", "dotdash", "solid", "dashed", "dotdash")
+  do.call(eafplot, args)
+  return(data)
+}
---
+++ tests/testthat/test_plotEAF.R
@@ -0,0 +1,32 @@
+context("plotEAF")
+
+test_that("plotEAF works", {
+
+  obj.fun = makeMBOFunction(function(x) c(1, -1) * x^2)
+  par.set = makeNumericParamSet(len = 2L, lower = -2, upper = 1)
+
+  # Test normal run
+  learner = makeLearner("regr.km", nugget.estim = TRUE)
+  ctrl = makeMBOControl(iters = 2, number.of.targets = 2L, init.design.points = 5L)
+  ctrl = setMBOControlInfill(ctrl, opt.focussearch.points = 10)
+  ctrl = setMBOControlMultiCrit(ctrl, method = "parego", parego.s = 100)
+
+  # build aritificial list of opt pathes for two algorithms
+  n.reps = 3L
+  opt.pathes = lapply(seq(n.reps), function(i) {
+    set.seed(i)
+    res = mbo(f, ps, learner = learner, control = ctrl)
+    return(res$opt.path)
+  })
+  opt.pathes = list("algo1" = opt.pathes, "algo2" = opt.pathes)
+
+  # plot eaf and check returned data frame
+  res = plotEAF(opt.pathes)
+
+  algo.names = c("algo1", "algo2")
+
+  expect_true(is.data.frame(res))
+  expect_true(setequal(colnames(res), c("y_1", "y_2", ".algo", ".repl")))
+  expect_true(all(res$.algo %in% algo.names))
+  expect_true(all(res$.repl >= 1) && all(res$.repl <= n.reps))
+})
