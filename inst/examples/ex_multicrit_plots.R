25aebf452cc90d4dd133b7988887f6e3d710f4d9
Jakob Bossek <info@jakobbossek.de>
2014-12-16 14:57:00 +0100

    improve and export plot method for MBOMultiCritResult objects

---
+++ inst/examples/ex_multicrit_plots.R
@@ -0,0 +1,18 @@
+library(mco)
+library(mlrMBO)
+
+set.seed(1)
+
+obj.fun = makeMBOFunction(mco::zdt1)
+par.set = makeNumericParamSet(len = 5L, lower = 0, upper = 1)
+
+lrn = makeLearner("regr.km", predict.type = "se")
+ctrl = makeMBOControl(iters = 10L, number.of.targets = 2L, init.design.points = 8L,
+  propose.points = 2L)
+ctrl = setMBOControlInfill(ctrl, crit = "ei", opt.focussearch.points = 1000L,
+  opt.focussearch.maxit = 3L)
+ctrl = setMBOControlMultiCrit(ctrl, method = "parego")
+
+or = mbo(obj.fun, par.set, learner = lrn, control = ctrl, show.info = TRUE)
+
+plot(or, alpha = TRUE, infill.crit = "ei", log.infill.crit = FALSE, pause = TRUE)
