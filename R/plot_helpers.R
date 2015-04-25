e1a8692a7ce9d4ae686049483dfa4e1e44e787e2
Karin Schork <schork.karin@gmail.com>
2015-04-25 12:12:21 +0200

    plot_helpers: typos in docu

--- R/plot_helpers.R
+++ R/plot_helpers.R
@@ -1,12 +1,12 @@
 
-# checks or sets lim.x and lim.y for mbo Plots. lim.x and lim.y are names lists,
+# Checks or sets lim.x and lim.y for mbo Plots. lim.x and lim.y are named lists,
 # with names corresponding to the plots (XSPace, YSpace, CritPlot, HVPlot, 
-# ExtraPlot1, ExtraPlot2), each list element must be NULL or numeric of length 2
-# if NULL, the corresponding lims are calculated via the range of the corresponding
-# values
+# ExtraPlot1, ExtraPlot2), each list element must be NULL or numeric of length 2.
+# If NULL, the corresponding limits are calculated via the range of the corresponding
+# values.
 
 # X limits for diagnostic plots will NOT be set. Since we have the number of
-# iterations on the X axis here, the ggplot default always do what we want
+# iterations on the X axis here, the ggplot default always does what we want.
 
 getLimits <- function(lim.x, lim.y, result, iters, extra.measure, ref.point) {
   op = result$opt.path
@@ -20,14 +20,14 @@ getLimits <- function(lim.x, lim.y, result, iters, extra.measure, ref.point) {
   # Crit Plot
   # FIXME MSPOT is special
   
-  # lim.x is iteration number here. if NULL, use ggplot defaults, else check
+  # lim.x is the iteration number here. If NULL, use ggplot defaults, else check.
   if (is.null(lim.x$CritPlot)) {
     lim.x$CritPlot = c(NA_real_, NA_real_)
   } else {
     assertNumeric(lim.x$CritPlot, len = 2)
   }
   
-  # if NULL, use range of observed crit, else check
+  # If NULL, use range of observed crit, else check.
   if (is.null(lim.y$CritPlot)) {
     vals = na.omit(getOptPathCol(op, ctrl$infill.crit, dob = 0:max(iters)))
     if (length(vals) == 0L) {
@@ -44,16 +44,16 @@ getLimits <- function(lim.x, lim.y, result, iters, extra.measure, ref.point) {
   # HV Plot
   # iteration
   if (ctrl$number.of.target > 1L) {
-    # lim.x is iteration number here. if NULL, use ggplot defaults, else check
+    # lim.x is iteration number here. If NULL, use ggplot defaults, else check.
     if (is.null(lim.x$HVPlot)) {
       lim.x$HVPlot = c(NA_real_, NA_real_)
     } else {
       assertNumeric(lim.x$HVPlot, len = 2)
     }
     
-    # if null, use min and max of hypervolume, else check
+    # If null, use min and max of hypervolume, else check.
     if (is.null(lim.y$HVPlot)) {
-      # HV is monotonically increasing - just get the value for first and last
+      # HV is monotonically increasing - just get the value for first and last.
       hv.min = getDominatedHV(getOptPathParetoFront(op = op, dob = 0), ref.point, ctrl$minimize)
       hv.max = getDominatedHV(getOptPathParetoFront(op = op, dob = 0:max(iters)), ref.point, ctrl$minimize)
       lim.y$HVPlot = c(hv.min, hv.max)
@@ -64,15 +64,15 @@ getLimits <- function(lim.x, lim.y, result, iters, extra.measure, ref.point) {
   
   # Extra Measure Plots
   if (length(extra.measure) > 0L) {
-    # lim.x is iteration number here. if NULL, use ggplot defaults, else check
+    # lim.x is iteration number here. If NULL, use ggplot defaults, else check.
     if (is.null(lim.x$ExtraPlot1)) {
-      # If NULL, we do nothing and use the ggplot defaults later
+      # If NULL, we do nothing and use the ggplot defaults later.
       lim.x$ExtraPlot1 = c(NA_real_, NA_real_)
     } else {
       assertNumeric(lim.x$ExtraPlot1, len = 2)
     }
     
-    # if NULL, use range of observed measure, else check
+    # If NULL, use range of observed measure, else check.
     if (is.null(lim.y$ExtraPlot1)) {
       vals = na.omit(getOptPathCol(op, extra.measure[1], dob = 0:max(iters)))
       if (length(vals) == 0L) {
@@ -87,14 +87,14 @@ getLimits <- function(lim.x, lim.y, result, iters, extra.measure, ref.point) {
   }
   
   if (length(extra.measure) > 1L) {
-    # If NULL, we do nothing and use the ggplot defaults later
+    # If NULL, we do nothing and use the ggplot defaults later.
     if (is.null(lim.x$ExtraPlot2)) {
       lim.x$ExtraPlot2 = c(NA_real_, NA_real_)
     } else {
       assertNumeric(lim.x$ExtraPlot2, len = 2)
     }
     
-    # if NULL, use range of observed measure, else check
+    # If NULL, use range of observed measure, else check.
     if (is.null(lim.y$ExtraPlot2)) {
       vals = na.omit(getOptPathCol(op, extra.measure[2], dob = 0:max(iters)))
       if (length(vals) == 0L) {
