9728b281bff42afbdf92f963bdb2ed3104cf360a
Jakob Richter <code@jakob-r.de>
2015-06-11 11:44:45 +0200

    multifid test up to date and running

--- tests/testthat/test_multifid.R
+++ tests/testthat/test_multifid.R
@@ -1,7 +1,7 @@
 context("multifid")
 
 test_that("basic multifid works", {
-  
+  set.seed(21)
   objfun = function(x, fac = 0.5) {
     lvl.par.val = x$.multifid.lvl / 3
     x = x$x
@@ -13,33 +13,33 @@ test_that("basic multifid works", {
   }
 
   par.set = makeParamSet(
-    makeNumericParam("x", lower = 0, upper = 10),
-    makeNumericParam("dw.perc", lower = 0, upper = 1)
+    makeNumericParam("x", lower = 0, upper = 10)
   )
 
   control = makeMBOControl(
     init.design.points = 9L,
     init.design.fun = maximinLHS,
-    iters = 3L,
+    iters = 10L,
     on.learner.error = "stop",
     show.learner.output = FALSE,
   )
   control = setMBOControlInfill(control = control, 
-    crit = "ei",
-    opt = "focussearch",
-    opt.restarts = 1L,
-    opt.focussearch.maxit = 1L,
-    opt.focussearch.points = 200L,
-    filter.proposed.points = TRUE,
-    filter.proposed.points.tol = 0.01
+                                crit = "ei",
+                                opt = "focussearch",
+                                opt.restarts = 1L,
+                                opt.focussearch.maxit = 1L,
+                                opt.focussearch.points = 50L,
+                                filter.proposed.points = TRUE,
+                                filter.proposed.points.tol = 0.01
   )
   control = setMBOControlMultiFid(control = control,
-    param = "dw.perc",
-    lvls = c(0.1, 0.5, 1),
-    cor.grid.points = 40L)
+                                  param = "dw.perc",
+                                  costs = 1:3,
+                                  lvls = c(0.1, 0.5, 1),
+                                  cor.grid.points = 40L)
 
-  surrogat.learner = makeLearner("regr.km", nugget.estim = TRUE, jitter = TRUE)
+  surrogat.learner = makeLearner("regr.km", nugget.estim = TRUE, jitter = TRUE, predict.type = "se")
   result = mbo(fun = objfun, par.set = par.set, learner = surrogat.learner, control = control)
-  expect_true(inherits(result, "MultiFidResult"))
+  expect_true(abs(result$x$x - 7.864932) < 0.2)
 })
 
