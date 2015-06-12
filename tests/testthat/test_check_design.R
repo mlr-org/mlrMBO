0a7e9bfab9d426d336651a6ac7a85108f5b13914
Michel <michellang@gmail.com>
2015-06-12 13:14:44 +0200

    cleanup cruft top level files

--- tests/testthat/test_check_design.R
+++ tests/testthat/test_check_design.R
@@ -8,17 +8,17 @@ test_that("test checks for initial design and new design", {
     makeNumericParam("y", lower = 1, upper = 2, requires = quote(w == "b")),
     makeNumericParam("z", lower = 1, upper = 2, requires = quote(w == "e"))
   )
-  
+
   design1 = generateDesign(8, par.set)
   expect_false(checkInitDesign(design1, par.set))
-  
+
   design2 = generateDesign(30, par.set)
   expect_true(checkInitDesign(design2, par.set))
-  
+
   checked = checkPredictionData(design2, design1)
   expect_true(any(checked))
   expect_true(any(!checked))
-  
+
   design = data.frame(x = c("a", "b"))
   new.design = data.frame(x = c("a", "c", "b", "d", "a", NA))
   expect_equal(checkPredictionData(new.design, design),
