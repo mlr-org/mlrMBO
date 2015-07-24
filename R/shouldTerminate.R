37e34854b9ad931571be64c9f192d63de7ddd192
Jakob Richter <code@jakob-r.de>
2015-07-24 14:13:37 +0200

    shouldTerminate directly on OptState

--- R/shouldTerminate.R
+++ R/shouldTerminate.R
@@ -1,35 +1,25 @@
 # Helper which checks stopping criteria.
 #
-# @param max.iters [\code{integer(1)} | NULL]\cr
-#   Maximal number of iterations.
-# @param iter [\code{integer(1)}]\cr
-#   Current iteration.
-# @param time.budget [\code{integer(1)}]\cr
-#   Time budget in seconds.
-# @param start.time [\code{POSIXct}]
-#   Starting time of mbo agorithm (result of Sys.time() call).
-# @param exec.time.budget [\code{integer(1)}]\cr
-#   Time budget in seconds for execution (read from opt.path).
-# @param target.fun.value [\code{numeric(1)}]
-#   Optimal function value to reach
-# @param minimize [\code{logical}]
-#   Minimize value from control
-# @param opt.path [\code{OptPath}]\cr
-# @return [\code{integer(1)}] Negative value is no stopping criterion is satisfied. 0 if
-#   maximum number of iterations is reached, 1 if time budget is exceeded and 3 if
-#   targete function value is reached
-## FIXME: documentation for 2 is missing
-shouldTerminate = function(max.iters, iter, time.budget, start.time, exec.time.budget,
-  target.fun.value, minimize, opt.path = NULL, show.info = FALSE) {
-  if (iter > max.iters){
-    showInfo(show.info, "max.iters %i reached with %i", max.iters, iter)
-  	return(1L)
+# @param opt.state [\code{OptState}]\cr
+
+shouldTerminate.OptState = function(opt.state) {
+  opt.problem = getOptStateOptProblem(opt.state)
+  control = getOptProblemControl(opt.problem)
+  iter = getOptStateLoop(opt.state)
+  start.time = getOptProblemStartTime(opt.problem)
+  target.fun.value = control$target.fun.value
+  minimize = control$minimize
+  opt.path = getOptStateOptPath(opt.state)
+
+  if (iter > control$iters){
+    showInfo(getOptProblemShowInfo(opt.problem), "max.iters %i reached with %i", control$iters, iter)
+    return(1L)
   }
-  if (isTimeBudgetExceeded(start.time, time.budget)) {
-  	return(2L)
+  if (isTimeBudgetExceeded(start.time, control$time.budget)) {
+    return(2L)
   }
-  if (isTimeBudgetExceeded(opt.path, exec.time.budget)) {
-  	return(3L)
+  if (isTimeBudgetExceeded(opt.path, control$exec.time.budget)) {
+    return(3L)
   }
   # target.fun.value only useful for single crit
   if (length(minimize) == 1L) {
@@ -40,23 +30,6 @@ shouldTerminate = function(max.iters, iter, time.budget, start.time, exec.time.b
     }
   }
   return(0)
-}
-
-shouldTerminate.OptState = function(opt.state) {
-  opt.problem = getOptStateOptProblem(opt.state)
-  control = getOptProblemControl(opt.problem)
-  shouldTerminate(
-    max.iters = control$iters,
-    iter = getOptStateLoop(opt.state),
-    time.budget = control$time.budget,
-    start.time = getOptProblemStartTime(opt.problem),
-    exec.time.budget = control$exec.time.budget,
-    target.fun.value = control$target.fun.value,
-    minimize = control$minimize,
-    opt.path = getOptStateOptPath(opt.state),
-    show.info = getOptProblemShowInfo(opt.problem)
-  )
-
 }
 
 # This function returns all the character which lead to an termination 
