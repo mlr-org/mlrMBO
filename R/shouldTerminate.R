2dde5586fe3da92a758c3135b1f9b9cd8d67a90b
Jakob Richter <code@jakob-r.de>
2015-10-29 12:07:26 +0100

    shouldTerminate was merged wrong, now again on opt.state

--- R/shouldTerminate.R
+++ R/shouldTerminate.R
@@ -1,31 +1,18 @@
 # Helper which checks stopping criteria.
 #
 # @param opt.state [\code{OptState}]\cr
-#   Maximal number of iterations.
-# @param iter [\code{integer(1)}]\cr
-#   Current iteration.
-# @param time.budget [\code{integer(1)}]\cr
-#   Time budget in seconds.
-# @param start.time [\code{POSIXct}]
-#   Starting time of mbo agorithm (result of Sys.time() call).
-# @param exec.time.budget [\code{integer(1)}]\cr
-#   Time budget in seconds for execution (read from opt.path).
-# @param target.fun.value [\code{numeric(1)}]
-#   Optimal function value to reach
-# @param minimize [\code{logical}]
-#   Minimize value from control
-# @param opt.path [\code{OptPath}]\cr
-# @return [\code{integer(1)}] Negative value is no stopping criterion is satisfied. 0 if
-#   maximum number of iterations is reached, 1 if time budget is exceeded and 3 if
-#   targete function value is reached
-## FIXME: documentation for 2 is missing
-shouldTerminate = function(max.iters, iter, time.budget, start.time, exec.time.budget,
-  target.fun.value, minimize, opt.path = NULL, show.info = FALSE) {
-  if (iter > max.iters){
-    showInfo(show.info, "max.iters %i reached with %i", max.iters, iter)
-  	return(0L)
-  }
-  if (isTimeBudgetExceeded(start.time, time.budget)) {
+
+shouldTerminate.OptState = function(opt.state) {
+  opt.problem = getOptStateOptProblem(opt.state)
+  control = getOptProblemControl(opt.problem)
+  iter = getOptStateLoop(opt.state)
+  start.time = getOptProblemStartTime(opt.problem)
+  target.fun.value = control$target.fun.value
+  minimize = control$minimize
+  opt.path = getOptStateOptPath(opt.state)
+
+  if (iter > control$iters){
+    showInfo(getOptProblemShowInfo(opt.problem), "max.iters %i reached with %i", control$iters, iter)
     return(1L)
   }
   if (isTimeBudgetExceeded(start.time, control$time.budget)) {
