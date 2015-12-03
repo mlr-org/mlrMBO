c46c4b595efad1b4b012f95f910ddc4ffb84f8a7
Jakob Richter <code@jakob-r.de>
2015-12-03 10:08:46 +0100

    fix tests and manpages

--- tests/testthat/test_smart_schedule.R
+++ tests/testthat/test_smart_schedule.R
@@ -3,10 +3,9 @@ context("smart schedule")
 test_that("smart schedule works", {
   set.seed(2)
   objfun = function(x) {
-    x = x$x
-    assertNumeric(x, len = 1, lower = 0, upper = 10)
-    Sys.sleep(0.25*(sin(x)+1)+0.01)
-    (x-3)^2 + 10*sin(x*2)
+    x = (x$x-5)^2
+    attr(x,"exec.time") = 10L
+    return(x)
   }
   
   par.set = makeParamSet(
@@ -21,14 +20,14 @@ test_that("smart schedule works", {
     stringsAsFactors = FALSE
     )
   
-  surrogat.learner = makeLearner("regr.km", predict.type = "se", nugget.estim = TRUE)
+  surrogat.learner = makeLearner("regr.randomForest", predict.type = "se", ntree = 10)
   ors = rowLapply(setups, function(x) {
     control = makeMBOControl(
       init.design.points = 5L,
       iters = 2L,
-      propose.points = 9L,
+      propose.points = 5L,
       schedule.method = "smartParallelMap",
-      schedule.nodes = 3L,
+      schedule.nodes = 5L,
       schedule.priority = x$schedule.priority,
       schedule.priority.time = x$schedule.priority.time
     )
@@ -36,14 +35,15 @@ test_that("smart schedule works", {
     control = setMBOControlMultiPoint(control = control, lcb.multiple = x$multipoint.lcb.multiple)
     or = mbo(fun = objfun, par.set = par.set, learner = surrogat.learner, control = control)
     op.df = as.data.frame(or$opt.path)
-    expect_true(!all(is.na(op.df$predicted.time)))
+    expect_true(nrow(op.df) == 15)
+    expect_true(all(na.omit(op.df$predicted.time) %btwn% c(9,11)))
     expect_true(!all(is.na(op.df$predicted.time.se)))
     expect_true(!all(is.na(op.df$scheduled.at)))
     expect_true(!all(is.na(op.df$scheduled.on)))
     expect_true(!all(is.na(op.df$scheduled.job)))
     expect_true(!all(is.na(op.df$scheduled.priority)))
     expect_true(!all(is.na(op.df$lcb.lambda)))
-    expect_true(abs(mean(op.df$lcb.lambda, na.rm = TRUE) - x$crit.lcb.lambda) < 2)
+    expect_true(abs(mean(op.df$lcb.lambda, na.rm = TRUE) - x$crit.lcb.lambda) < sd(op.df$lcb.lambda, na.rm = TRUE))
     or
   })
 })
