83098dc4ee2677e13095fec3e38ecb536da03d56
Michel <michellang@gmail.com>
2016-02-12 10:30:27 +0100

    refactored checkDesign

--- R/checkDesign.R
+++ R/checkDesign.R
@@ -1,24 +1,22 @@
-
-# Check if the initial design is large enoug. in detail:
-# every factor level is "visited" for factors
-# and every dependet feature has NAs and not-NAs
-
+# Check if the initial design is large enough. I.e.:
+# Every factor level is "visited" for factors
+# and every dependent feature has NAs and not-NAs
 checkInitDesign = function(design, par.set) {
-  # discrete Params
-  # all levels are visited
+  # discrete Params: all levels are visited
   disc.params = filterParams(par.set, type = "discrete")
-  disc.params.values = getValues(disc.params)
-  disc.design = design[getParamIds(disc.params)]
-  disc.design.values = apply(disc.design, 2, unique)
-  disc.ok = sapply(seq_along(disc.params.values), function(i)
-    all(disc.params.values[[i]] %in% disc.design.values[[i]]))
-  
+  par.ids = getParamIds(disc.params)
+  disc.params.values = lapply(getValues(disc.params), unlist, use.names = FALSE)
+  not.visited = Map(setdiff, disc.params.values[par.ids], design[par.ids])
+  if (any(lengths(not.visited) > 0L))
+    return(FALSE)
+
   # if the param has requires, check if NA and normal level is visited
-  has.requires = sapply(par.set$pars, hasRequires)
-  requires.design = design[, has.requires]
-  requires.ok = apply(is.na(requires.design), 2, function(x) any(x) && any(!x))
-  
-  return(all(disc.ok, requires.ok))
+  has.requires = vlapply(par.set$pars, hasRequires)
+  sum.na = lapply(design[has.requires], function(x) sum(is.na(x)))
+  if (any(sum.na == 0 | sum.na == nrow(design)))
+    return(FALSE)
+
+  return(TRUE)
 }
 
 # Check if a line of the new design is able to predict atm
