0d3c92967395e5de518582f6a8e0c03981cd3261
Jakob Richter <code@jakob-r.de>
2016-02-12 17:30:24 +0100

    rebase, fix multifid test, WIP

--- tests/testthat/test_multifid.R
+++ tests/testthat/test_multifid.R
@@ -1,61 +1,60 @@
-# context("multifid")
+context("multifid")
 
-# test_that("basic multifid works", {
-#   f = makeSingleObjectiveFunction(
-#     fn = function(x) {
-#       lvl.par.val = x$.multifid.lvl
-#       x = x$x
-#       assertNumeric(x, len = 1, lower = 0, upper = 10)
-#       3 - lvl.par.val + 0.5 * x
-#     },
-#     par.set = makeParamSet(
-#       makeNumericParam("x", lower = 0, upper = 10)
-#     ),
-#     has.simple.signature = FALSE
-#   )
+test_that("basic multifid works", {
+  f = makeSingleObjectiveFunction(
+    fn = function(x) {
+      lvl.par.val = x$.multifid.lvl
+      x = x$x
+      assertNumeric(x, len = 1, lower = 0, upper = 10)
+      3 - lvl.par.val + 0.5 * x
+    },
+    par.set = makeParamSet(
+      makeNumericParam("x", lower = 0, upper = 10),
+      makeIntegerParam(".multifid.lvl", lower = 1L, upper = 3L)
+    ),
+    has.simple.signature = FALSE
+  )
 
-#   control = makeMBOControl(
-#     on.learner.error = "stop",
-#     show.learner.output = FALSE,
-#   )
-#   control = setMBOControlTermination(control, iters = 5L)
+  control = makeMBOControl()
+  control = setMBOControlTermination(control, iters = 20L)
 
-#   control = setMBOControlInfill(control = control,
-#                                 crit = "ei",
-#                                 opt = "focussearch",
-#                                 opt.restarts = 1L,
-#                                 opt.focussearch.maxit = 1L,
-#                                 opt.focussearch.points = 10L,
-#                                 filter.proposed.points = TRUE,
-#                                 filter.proposed.points.tol = 0.01
-#   )
-#   control = setMBOControlMultiFid(control = control,
-#                                   param = "dw.perc",
-#                                   costs = 1:3,
-#                                   lvls = c(0.1, 0.5, 1),
-#                                   cor.grid.points = 40L)
+  control = setMBOControlInfill(control = control,
+                                crit = "ei",
+                                opt = "focussearch",
+                                opt.restarts = 1L,
+                                opt.focussearch.maxit = 1L,
+                                opt.focussearch.points = 10L,
+                                filter.proposed.points = TRUE,
+                                filter.proposed.points.tol = 0.01
+  )
+  control = setMBOControlMultiFid(control = control,
+                                  param = "dw.perc",
+                                  costs = 1:3,
+                                  lvls = c(0.1, 0.5, 1),
+                                  cor.grid.points = 40L)
 
-#   surrogat.learner = makeLearner("regr.lm", predict.type = "se")
-#   des = generateTestDesign(10L, getParamSet(f))
-#   result = mbo(f, des, learner = surrogat.learner, control = control)
-#   expect_true(result$y < 0.5)
+  surrogat.learner = makeLearner("regr.lm", predict.type = "se")
+  set.seed(1)
+  des = generateTestDesign(10L, getParamSet(f), fun = lhs::maximinLHS)
+  result = mbo(f, des, learner = surrogat.learner, control = control)
+  expect_true(result$y < 0.5)
 
-#   # remove cots so time.model will be estimated
-#   control$multifid.costs = NULL
-#   f.delay = makeSingleObjectiveFunction(
-#     fn = function(x) {
-#       Sys.sleep(0.1 + x$.multifid.lvl/3)
-#       f(x)
-#     },
-#     par.set = makeParamSet(
-#       makeNumericParam("x", lower = 0, upper = 10)
-#     ),
-#     has.simple.signature = FALSE
-#   )
-#   set.seed(1)
-#   result.time = mbo(f.delay, learner = surrogat.learner, control = control)
+  # remove cots so time.model will be estimated
+  control$multifid.costs = NULL
+  f.delay = makeSingleObjectiveFunction(
+    fn = function(x) {
+      Sys.sleep(0.1 + x$.multifid.lvl/3)
+      return(f(x))
+    },
+    par.set = makeParamSet(
+      makeNumericParam("x", lower = 0, upper = 10),
+      makeIntegerParam(".multifid.lvl", lower = 1L, upper = 3L)
+    ),
+    has.simple.signature = FALSE
+  )
+  result.time = mbo(f.delay, des, learner = surrogat.learner, control = control)
 
-#   # this is pretty hard and migh fail?
-#   expect_equal(as.data.frame(result.time$opt.path)$.multifid.lvl, as.data.frame(result$opt.path)$.multifid.lvl)
-# })
+  # this is pretty hard and migh fail?
+  #expect_equal(as.data.frame(result.time$opt.path)$.multifid.lvl, as.data.frame(result$opt.path)$.multifid.lvl)
+})
 
