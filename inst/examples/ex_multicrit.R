3b210c4982643d85b240c2c68faddf260f1ecac9
Jakob Bossek <info@jakobbossek.de>
2016-02-15 15:15:56 +0100

    remove ref.point from exampleRunMultiCrit

--- inst/examples/ex_multicrit.R
+++ inst/examples/ex_multicrit.R
@@ -23,6 +23,6 @@ ctrl = setMBOControlMultiCrit(ctrl, parego.s = 100)
 design = generateDesign(5L, getParamSet(obj.fun), fun = lhs::maximinLHS)
 
 run = exampleRunMultiCrit(obj.fun, design = design, learner = learner, ctrl, points.per.dim = 50L,
-  show.info = TRUE, nsga2.args = list(), ref.point = c(11, 11))
+  show.info = TRUE, nsga2.args = list())
 
 plotExampleRun(run, pause = pause)
