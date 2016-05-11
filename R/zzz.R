20871dd6ec96711c0ef80d3aacbfd1e5b2b86962
Bernd Bischl <bernd_bischl@gmx.net>
2016-05-11 21:24:49 +0200

    fix imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,14 @@
 #' @import BBmisc
 #' @import checkmate
 #' @import ggplot2
+#' @import grDevices
 #' @import lhs
 #' @import mlr
 #' @import parallelMap
 #' @import ParamHelpers
 #' @import smoof
+#' @import stats
+#' @import utils
 #' @useDynLib mlrMBO c_sms_indicator c_eps_indicator
 NULL
 
