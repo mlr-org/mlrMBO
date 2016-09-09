e3f3acb3d19cf4a4f0184705c8873f9af3aa9a43
Jakob Richter <code@jakob-r.de>
2016-09-09 16:09:30 +0200

    stil early discreteWrapper

--- R/DiscreteWrapper.R
+++ R/DiscreteWrapper.R
@@ -5,37 +5,46 @@
 #'
 #' @param learner [\code{Learner}]\cr
 #'   mlr Learner
-#' @param aw.quantiles [\code{numeric}]\cr
-#'   Quantiles that should be calculated for imputation.
-#'   Default is \code{c(0.25,0.5, 0.75)}.
+#' @param discw.category [\code{charachter}]\cr
+#'   names of the columns that indicate the categorical value to split before training.
+#'   If value is NULL (default) then the first categorical value will be taken into account.
 #' @return Wrapped Learner
 #' @family wrapper
 #' 
-#' 
+#' @export
 
-makeDiscreteWrapper = function(learner) {
+makeDiscreteWrapper = function(learner, discw.category = NULL, discw.na.val = NULL) {
 
-  myTrafo = function(data, target, category, na.val) {
-  catf("run mytrafon on data with")
-    
-    data = cbind(dcast(data.notarget, as.formula(paste0("seq_along(", category, ")~",category)), fill = na.val, value.var = "x", drop = FALSE)[, -1, drop = FALSE], data[,colnames(data) %in% c(category, target), drop = FALSE])
-    return(data)
-  }
+  args = list(
+    discw.category = discw.category,
+    discw.na.val = discw.na.val)
+
+  rm(list = names(args))
 
   trainfun = function(data, target, args) {
     data.notarget = data[, colnames(data) != target, drop = FALSE]
-    data.notarget = cbind(dcast(data.notarget, as.formula(paste0("seq_along(", category, ")~",category)), fill = na.val, value.var = "x", drop = FALSE)[, -1, drop = FALSE], data[,colnames(data) %in% c(category, target), drop = FALSE])
+    discw.category = coalesce(
+      args$discw.category,
+      names(which(sapply(data.notarget, is.factor)))[1])
+    discw.na.val = coalesce(
+      args$discw.na.val,
+      max(as.matrix(data.notarget[, sapply(data.notarget, is.numeric)])) * 10
+      )
+    data = discreteWrapperTrafoHelper(data = data, target = target, category = discw.category, na.val = args$na.val)
+    list(data = data, control = list(discw.category = discw.category, discw.na.val = discw.na.val))
   }
 
-  wrpTrain = function(data, target, args) {
-    data = myTrafo(data = data, target = target, category = args$category, na.val = args$na.val)
-    list(data = data, control = list())
+  predictfun = function(data, target, args, control) {
+    discreteWrapperTrafoHelper(data = data, target = target, category = control$discw.category, na.val = control$discw.na.val)
   }
 
-  wrpPredict = function(data, target, args, control) {
-    myTrafo(data = data, target = target, category = args$category, na.val = args$na.val)
-  }
-
-  lrn = makePreprocWrapper(learner = learner, train = wrpTrain, predict = wrpPredict, par.vals = list(category = "category", na.val = 1024))
+  lrn = makePreprocWrapper(learner = learner, train = trainfun, predict = predictfun, par.vals = args)
   addClasses(lrn, "DiscreteWrapper")
+}
+
+discreteWrapperTrafoHelper = function(data, target, category, na.val) {
+  data.notarget = data[, colnames(data) != target, drop = FALSE]
+  cbind(
+    reshape2::dcast(data.notarget, as.formula(paste0("seq_along(", category, ")~",category)), fill = na.val, value.var = "x", drop = FALSE)[, -1, drop = FALSE], 
+    data[,colnames(data) %in% c(category, target), drop = FALSE])
 }
