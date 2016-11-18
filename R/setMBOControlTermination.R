d82dcbc768514a202efccd1452b0e76756b47bdd
Jakob Richter <code@jakob-r.de>
2016-11-18 11:31:46 +0100

    abolish stopping in favor of termination (#271)

--- R/setMBOControlTermination.R
+++ R/setMBOControlTermination.R
@@ -16,27 +16,27 @@
 #'   the condition is checked after each iteration.
 #'   The default \code{NULL} means: There is no execution time budget.
 #' @param target.fun.value [\code{numeric(1)}] | NULL]\cr
-#'   Stopping criterion for single crit optimization: Stop if a function evaluation
+#'   Termination criterion for single crit optimization: Stop if a function evaluation
 #'   is better than this given target.value.
-#'   The default \code{NULL} means: The function value won't be taken into account for stopping.
+#'   The default \code{NULL} means: The function value won't be taken into account for termination.
 #' @param max.evals [\code{integer(1) | NULL}]\cr
 #'   Maximal number of function evaluations.
-#'   The default \code{NULL} means: The total number of evaluations won't be taken into account for stopping.
-#' @param more.stop.conds [\code{list}]\cr
+#'   The default \code{NULL} means: The total number of evaluations won't be taken into account for termination.
+#' @param more.termination.conds [\code{list}]\cr
 #'   Optional list of termination conditions. Each condition needs to be a function
 #'   of a single argument \code{opt.state} of type \code{\link{OptState}} and should
 #'   return a list with the following elements:
 #'   \describe{
 #'     \item{term [\code{logical(1)}]}{Logical value indicating whether the
-#'     stopping condition is met.}
-#'     \item{message [\code{character(1)}]}{Stopping message.}
+#'     termination condition is met.}
+#'     \item{message [\code{character(1)}]}{Termination message. At the moment we just allow \code{term.custom}.}
 #'   }
 #' @return [\code{\link{MBOControl}}].
 #' @examples
 #' fn = smoof::makeSphereFunction(1L)
 #' ctrl = makeMBOControl()
 #'
-#' # custom stopping condition (stop if target function value reached)
+#' # custom termination condition (stop if target function value reached)
 #' # We neglect the optimization direction (min/max) in this example.
 #' yTargetValueTerminator = function(y.val) {
 #'   force(y.val)
@@ -49,21 +49,21 @@
 #'   }
 #' }
 #'
-#' # assign custom stopping condition
-#' ctrl = setMBOControlTermination(ctrl, more.stop.conds = list(yTargetValueTerminator(0.05)))
+#' # assign custom termination condition
+#' ctrl = setMBOControlTermination(ctrl, more.termination.conds = list(yTargetValueTerminator(0.05)))
 #' res = mbo(fn, control = ctrl)
 #' @family MBOControl
 #' @export
 setMBOControlTermination = function(control,
-  iters = NULL, time.budget = NULL, exec.time.budget = NULL, target.fun.value = NULL, max.evals = NULL, more.stop.conds = list()) {
+  iters = NULL, time.budget = NULL, exec.time.budget = NULL, target.fun.value = NULL, max.evals = NULL, more.termination.conds = list()) {
 
-  assertList(more.stop.conds)
+  assertList(more.termination.conds)
 
-  stop.conds = more.stop.conds
+  stop.conds = more.termination.conds
 
   if (is.null(iters) && is.null(time.budget) && is.null(exec.time.budget) && is.null(max.evals) && length(stop.conds) == 0L) {
     stopf("You need to specify a maximal number of iteration, a time budget or at least
-      one custom stopping condition, but you provided neither.")
+      one custom termination condition, but you provided neither.")
   }
 
   if (!is.null(iters)) {
@@ -88,7 +88,7 @@ setMBOControlTermination = function(control,
     stop.conds = c(stop.conds, makeMBOTerminationTargetFunValue(target.fun.value))
   }
 
-  # sanity check stopping conditions
+  # sanity check termination conditions
   lapply(stop.conds, function(stop.on) {
     assertFunction(stop.on, args = "opt.state")
   })
