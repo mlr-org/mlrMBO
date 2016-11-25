5cf089bd560f5427ccfe0b106b8501ba21c958d4
Jakob Bossek <info@jakobbossek.de>
2016-11-25 20:08:37 +0100

    fix ugly parallel LCB issue

--- R/proposePointsParallelCB.R
+++ R/proposePointsParallelCB.R
@@ -4,7 +4,7 @@ proposePointsParallelCB = function(opt.state) {
   control = getOptProblemControl(opt.problem)
 
   # draw lambdas from exp dist + create 1 control for each for single crit with lambda-CB
-  z = createRandomCBControls(control, "cb")
+  z = createRandomCBControls(control, makeMBOControlInfillCB())
 
   props = parallelMap(proposePointsByInfillOptimization, control = z$controls, level = "mlrMBO.propose.points",
     more.args = list(opt.state = opt.state))
