b1f4592674c7f40815a78f34c4324740efea60bf
Jakob Richter <code@jakob-r.de>
2017-01-26 13:00:53 +0100

    fix issue #317, prop.type for final evals (#319)
    * fix issue #317, prop.type for final evals
    
    * fix missed renaming

--- R/evalFinalPoint.R
+++ R/evalFinalPoint.R
@@ -1,11 +1,19 @@
 evalFinalPoint = function(opt.state, final.points) {
   opt.problem = getOptStateOptProblem(opt.state)
   control = getOptProblemControl(opt.problem)
-  final.evals = control$final.evals
+  n = control$final.evals
   # do some final evaluations and compute mean of target fun values
   # FIXME: Do we really want the resampling of the last point be part of the opt.path and thus be part of a new model fit if we restart the problem?
-  showInfo(getOptProblemShowInfo(opt.problem), "Performing %i final evals", final.evals)
-  xs = replicate(final.evals, final.points$x, simplify = FALSE)
-  extras = getExtras(final.evals, NULL, NA_real_, control)
-  evalTargetFun.OptState(opt.state, xs, extras)
+  showInfo(getOptProblemShowInfo(opt.problem), "Performing %i final evals", n)
+  xs = replicate(n, final.points$x, simplify = FALSE)
+  prop = list(
+    prop.points = xs,
+    crit.vals = matrix(rep.int(NA_real_, n), nrow = n, ncol = 1L),
+    crit.components = getMBOInfillCriterionDummyComponents(control$infill.crit),
+    propose.time = rep.int(NA_real_, n),
+    prop.type = rep("final_eval", n),
+    errors.model = rep.int(NA_character_, n)
+  )
+  extras = getExtras(n = n, prop = prop, train.time = NA_real_, control = control)
+  evalTargetFun.OptState(opt.state, xs = xs, extras = extras)
 }
