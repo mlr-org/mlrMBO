f3b669059b5138d246afb5f17ac5510d6947c364
Jakob Richter <code@jakob-r.de>
2017-02-02 17:58:13 +0100

    further work on clean plot code

--- inst/examples/ex_1d_discrete_noisy.R
+++ inst/examples/ex_1d_discrete_noisy.R
@@ -26,13 +26,13 @@ obj.fun = makeSingleObjectiveFunction(
   has.simple.signature = FALSE, # function expects a named list of parameter values
   noisy = TRUE
 )
-obj.fun = convertToMaximization(obj.fun)
 
-ctrl = makeMBOControl()
+ctrl = makeMBOControl(propose.points = 3)
+ctrl = setMBOControlMultiPoint(ctrl, method = "cb")
 ctrl = setMBOControlTermination(ctrl, iters = 10L)
 
 # we can basically do an exhaustive search in 3 values
-ctrl = setMBOControlInfill(ctrl, crit = makeMBOInfillCriterionCB(),
+ctrl = setMBOControlInfill(ctrl, crit = makeMBOInfillCriterionCB(cb.lambda = 5),
   opt.restarts = 1L, opt.focussearch.points = 3L, opt.focussearch.maxit = 1L)
 
 design = generateDesign(20L, getParamSet(obj.fun), fun = lhs::maximinLHS)
@@ -43,27 +43,51 @@ newExampleRun = function(fun, design = NULL, learner = NULL, control, show.info 
   control.mod = control
   control.mod$store.model.at = 1:1024L
   mbo.res = mbo(fun = fun, design = design, learner = learner, control = control.mod, show.info = show.info, more.args = more.args)
-  list(mbo.res = mbo.res, obj.fun = fun, control = control)
+  list(mbo.res = mbo.res, obj.fun = fun)
 }
 
 run = newExampleRun(obj.fun, design = design, learner = lrn, control = ctrl)
 
-resolution = 400
+resolution = 20
 
 # start plot
 obj.fun = run$obj.fun
 obj.fun.mean = coalesce(smoof::getMeanFunction(obj.fun), obj.fun)
 par.set = getParamSet(run$obj.fun)
-control = run$control
+control = run$mbo.res$control
 draw.design = generateGridDesign(par.set = par.set, resolution = resolution, trafo = FALSE)
 draw.design$y.real = vnapply(dfRowsToList(draw.design, par.set = par.set), obj.fun.mean)
+x.names = getParamIds(run$mbo.res$opt.path$par.set)
+x.types = getParamTypes(run$mbo.res$opt.path$par.set)
+x.name.numeric = x.names[x.types == "numeric"]
+x.name.discrete = x.names[x.types != "numeric"]
+y.names = run$mbo.res$opt.path$y.names
 
-iter = 1
+# opt path to data.frame
+op.df = as.data.frame(run$mbo.res$opt.path)
+
+# for one iteration
+iter = 3
 draw.design.iter = draw.design
 # predict the mean of the outcome
 this.model = run$mbo.res$models[[iter]]
+this.design = op.df[this.model$subset, this.model$features]
 prediction = predict(this.model, newdata = draw.design.iter)
 draw.design.iter$y.predict = getPredictionResponse(prediction)
 draw.design.iter$y.se = getPredictionSE(prediction)
 # get the infill crit value
-infill.fun = c
+infill.crit = control$infill.crit$fun(points = draw.design.iter, models = list(this.model), control = control, par.set = par.set, design = this.design, iter = iter, attributes = TRUE)
+draw.design.iter$infill.value = infill.crit
+draw.design.iter = cbind(draw.design.iter, attr(infill.crit, "crit.components"))
+
+# opt path for this iteration
+this.op.df = op.df[op.df$dob <= iter, ]
+this.op.df$variable = "y.predict"
+this.op.df$state = ifelse(this.op.df$dob == 0, "init", ifelse(this.op.df$dob == iter, "prop", "seq"))
+#plot the stuff
+mdata = reshape2::melt(draw.design.iter, measure.vars = c("y.predict", "infill.value"))
+g = ggplot()
+g = g + geom_line(data = mdata, mapping = aes_string(x = x.name.numeric, y = "value"))
+g = g + geom_line(data = mdata[mdata$variable == "y.predict",], mapping = aes_string(x = x.name.numeric, y = "y.real"), linetype = 2)
+g = g + geom_point(data = this.op.df, mapping = aes_string(x = x.name.numeric, y = y.names, shape = "state", color = "state"), size = 3)
+g + facet_grid(as.formula(sprintf("%s~%s", "variable", x.name.discrete)), scales = "free")
