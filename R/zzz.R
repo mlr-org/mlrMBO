78938420fd7a3b5c219d441868acd2af6529f266
Bernd Bischl <bernd_bischl@gmx.net>
2017-02-02 18:01:03 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -19,25 +19,17 @@ NULL
 # some shortcuts for infill criteria
 
 #' @rdname MBOInfillCriterion
-#' @section Predefined standard Infill Criteria:
-#' For commonly used settings of the Infill criteria:
+#' @section Predefined standard infill criteria:
 #' \describe{
 #' \item{crit.ei}{Expected Improvement}
-#' \item{crit.mr}{Mean Response}
-#' \item{crit.se}{Standard Error}
-#' \item{crit.cb}{Confidence Bound with Lambda dependent on Parameter Space}
-#' \item{crit.cb1}{Confidence Bound with Lambda=1}
-#' \item{crit.cb2}{Confidence Bound with Lambda=2}
-#' \item{crit.cb3}{Confidence Bound with Lambda=3}
-#' \item{crit.cb4}{Confidence Bound with Lambda=4}
-#' \item{crit.cb5}{Confidence Bound with Lambda=5}
+#' \item{crit.mr}{Mean response}
+#' \item{crit.se}{Standard error}
+#' \item{crit.cb}{Confidence bound with lambda dependent on parameter space}
+#' \item{crit.cb1}{Confidence bound with lambda=1}
+#' \item{crit.cb2}{Confidence bound with lambda=2}
 #' \item{crit.aei}{Augmeted expected improvement}
 #' \item{crit.eqi}{Expected quantile improvement}
-#' \item{crit.dib1}{Directed Indicator Based Search with Lambda=1}
-#' \item{crit.dib2}{Directed Indicator Based Search with Lambda=2}
-#' \item{crit.dib3}{Directed Indicator Based Search with Lambda=3}
-#' \item{crit.dib4}{Directed Indicator Based Search with Lambda=4}
-#' \item{crit.dib5}{Directed Indicator Based Search with Lambda=5}
+#' \item{crit.dib1}{Directed indicator based search with lambda=1}
 #' }
 #' 
 #' @rdname MBOInfillCriterion
@@ -88,27 +80,6 @@ crit.cb2 = makeMBOInfillCriterionCB(cb.lambda = 2)
 #' @docType NULL
 #' @format NULL
 #' @keywords NULL
-crit.cb3 = makeMBOInfillCriterionCB(cb.lambda = 3)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.cb4 = makeMBOInfillCriterionCB(cb.lambda = 4)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.cb5 = makeMBOInfillCriterionCB(cb.lambda = 5)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
 crit.aei = makeMBOInfillCriterionAEI()
 #' @rdname MBOInfillCriterion
 #' @export
@@ -124,31 +95,3 @@ crit.eqi = makeMBOInfillCriterionEQI()
 #' @format NULL
 #' @keywords NULL
 crit.dib1 = makeMBOInfillCriterionDIB(cb.lambda = 1)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.dib2 = makeMBOInfillCriterionDIB(cb.lambda = 2)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.dib3 = makeMBOInfillCriterionDIB(cb.lambda = 3)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.dib4 = makeMBOInfillCriterionDIB(cb.lambda = 4)
-#' @rdname MBOInfillCriterion
-#' @export
-#' @usage NULL
-#' @docType NULL
-#' @format NULL
-#' @keywords NULL
-crit.dib5 = makeMBOInfillCriterionDIB(cb.lambda = 5)
