99de38aad69bf7fa3b72f228098efeb1776ee76a
Bernd Bischl <bernd_bischl@gmx.net>
2017-02-02 18:19:56 +0100

    clean up mini stuff

--- tests/testthat/test_infillcrits.R
+++ tests/testthat/test_infillcrits.R
@@ -33,7 +33,7 @@ test_that("infill crits", {
       expect_true(!anyMissing(opdf$infill_ei[, c("ei","se","mean")]))
     if (!is.null(opdf$infill_cb)) {
       expect_true(!anyMissing(opdf$infill_cb[, c("se","mean","lambda")]))
-      expect_true(all(opdf$infill_cb$lambda == 5))
+      expect_true(all(opdf$infill_cb$lambda == 2))
     }
     if (!is.null(opdf$infill_aei))
       expect_true(!anyMissing(opdf$infill_aei[, c("se","mean","tau")]))
@@ -53,7 +53,7 @@ test_that("infill crits", {
   # we have converged and just waste time. we need to detect this somehow, or cope with it
   for (noisy in c(TRUE, FALSE)) {
     for (minimize in c(TRUE, FALSE)) {
-      crits = if (noisy) list(crit.aei, crit.eqi) else list(crit.mr, crit.se, crit.ei, crit.cb5)
+      crits = if (noisy) list(crit.aei, crit.eqi) else list(crit.mr, crit.se, crit.ei, crit.cb2)
       for (lrn in learners) {
         if (inherits(lrn, "regr.km"))
           lrn = setHyperPars(lrn, nugget.estim = noisy)
