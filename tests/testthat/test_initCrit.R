9f00a81010c8e26fabc74aea587fbf699b5dc017
Jakob Richter <code@jakob-r.de>
2017-02-13 13:14:14 +0100

    set opt.direction in test manually

--- tests/testthat/test_initCrit.R
+++ tests/testthat/test_initCrit.R
@@ -2,17 +2,17 @@ test_that("infill crit initialization", {
 
   fn = makeSphereFunction(2) # 2d continous space
 
-  expect_equal(crit.cb1, initCrit(crit.cb, fn))
+  expect_equal(setValue(crit.cb1, "opt.direction", "minimize"), initCrit(crit.cb, fn))
   expect_equal(crit.ei, initCrit(crit.ei, fn))
 
   crit.cb3 = makeMBOInfillCritCB(3)
 
-  expect_equal(crit.cb3, initCrit(crit.cb3, fn))
+  expect_equal(setValue(crit.cb3, "opt.direction", "minimize"), initCrit(crit.cb3, fn))
 
   fn = makeSwiler2014Function() # 2d continous space + 1 factor variable
 
-  expect_equal(crit.cb2, initCrit(crit.cb, fn))
-  expect_equal(crit.cb3, initCrit(crit.cb3, fn))
+  expect_equal(setValue(crit.cb2, "opt.direction", "minimize"), initCrit(crit.cb, fn))
+  expect_equal(setValue(crit.cb3, "opt.direction", "minimize"), initCrit(crit.cb3, fn))
 
 
 })
