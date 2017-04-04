3ef7456a092d0c393886fe19cec09f7ba705351e
Michel Lang <michellang@gmail.com>
2017-04-04 16:09:10 +0200

    cleanup

--- R/infill_crits.R
+++ R/infill_crits.R
@@ -112,10 +112,8 @@ makeMBOInfillCritCB = function(cb.lambda = NULL) {
   makeMBOInfillCrit(
     fun = function(points, models, control, par.set, design, iter, attributes = FALSE) {
       model = models[[1L]]
-      stopifnot(!is.null(model))
       maximize.mult = if (control$minimize) 1 else -1
       p = predict(model, newdata = points)$data
-      assertDataFrame(p, min.cols = 1)
       #FIXME: removed cb.inflate.se for now (see issue #309)
       # if (cb.inflate.se) {
       #   r.response = diff(range(p$response))
@@ -134,7 +132,6 @@ makeMBOInfillCritCB = function(cb.lambda = NULL) {
         res = setAttribute(res, "crit.components",
           data.frame(se = p$se, mean = p$response, lambda = cb.lambda))
       }
-      assertDataFrame(p, min.cols = 1)
       return(res)
     },
     name = "Confidence bound",
--- R/proposePointsWithRefinement.R
+++ R/proposePointsWithRefinement.R
@@ -1,15 +1,9 @@
 proposePointsWithRefinement = function(opt.state) {
+  # get result for un-refined optimization
   res = proposePointsByInfillOptimization(opt.state)
 
-  factor.values = as.list(res$prop.points[1L, ])
-  factor.values = factor.values[!vlapply(factor.values, is.numeric)]
+  # get various objects from the opt state
   control = getOptStateOptProblem(opt.state)$control
-
-  refinement.control = control$refinement$control
-  refinement.learner = control$refinement$learner
-  refinement.state = list2env(as.list(opt.state))
-  refinement.state$opt.problem = list2env(as.list(opt.state$opt.problem))
-
   fun = getOptProblemFun(opt.state)
   opt.problem = getOptStateOptProblem(opt.state)
   fun = getOptProblemFun(opt.problem)
@@ -17,21 +11,38 @@ proposePointsWithRefinement = function(opt.state) {
   learner = getOptProblemLearner(opt.problem)
   par.set = getOptProblemParSet(opt.problem)
 
+  # this is usually set in mbo/mboTemplate
+  # TODO: the structure here is not good.
+  refinement.control = control$refinement$control
   refinement.control$noisy = isNoisy(fun)
   refinement.control$minimize = shouldBeMinimized(fun)
-  refinement.learner = checkLearner(refinement.learner, par.set, refinement.control, fun)
-  refinement.learner = makeRefinementWrapper(refinement.learner, factor.values = factor.values)
-  refinement.control = checkStuff(fun, par.set, design, refinement.learner, refinement.control)
-  refinement.control$infill.crit = initCrit(refinement.control$infill.crit, fun, design, refinement.learner, refinement.control)
 
+  # construct the refinement learner
+  refinement.learner = control$refinement$learner
+  refinement.learner = checkLearner(refinement.learner, par.set, refinement.control, fun) # TODO: checkLearner should not set stuff
+  factor.values = as.list(res$prop.points[1L, ])
+  factor.values = factor.values[!vlapply(factor.values, is.numeric)]
+  refinement.learner = makeRefinementWrapper(refinement.learner, factor.values = factor.values)
+
+  # this is usually called in mbo/mboTemplate
+  refinement.control = checkStuff(fun, par.set, design, refinement.learner, refinement.control) # TODO: checkStuff should not set stuff
+  refinement.control$infill.crit = initCrit(refinement.control$infill.crit, fun, design, refinement.learner, refinement.control) # TODO: initCrit should not be necessary
+
+  # construct new opt.state
+  refinement.state = list2env(as.list(opt.state))
+  refinement.state$opt.problem = list2env(as.list(opt.state$opt.problem))
   refinement.state$opt.problem$learner = refinement.learner
   refinement.state$opt.problem$control = refinement.control
-  refinement.state$refinement = NULL
-  refinement.state$models = NULL
+  refinement.state$refinement = NULL # avoid recursion
+  refinement.state$models = NULL # disable caching
+
+  # propose with refinement
   new.res = proposePoints.OptState(refinement.state)
 
+  # patch old object with
   res$prop.points = new.res$prop.points
   res$propose.time = res$propose.time + new.res$propose.time
   res$prop.type = paste0(res$prop.type, "_refined_", new.res$prop.type)
+
   res
 }
