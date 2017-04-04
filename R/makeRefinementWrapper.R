dba86406b1db27b9d685ff8dc8a8566f74cdca8b
Michel Lang <michellang@gmail.com>
2017-04-04 15:59:54 +0200

    cleanup

--- R/makeRefinementWrapper.R
+++ R/makeRefinementWrapper.R
@@ -1,17 +1,26 @@
-#' @title Refinement Wrapper
-#'
-#' @template arg_learner
-#' @export
+# @title Refinement Wrapper
+#
+# @description
+# Wraps a mlr learner with a preprocessing wrapper:
+# The data is subsetted to (a) rows exactly matching the values provided in argument
+# \code{factor.values} and (b) to the numerical features.
+# This is used internally for Kriging Refinement.
+#
+# @param learner [\code{\link[mlr]{Learner}}]\cr
+#   A mlr learner.
+# @param factor.values [\code{named list}]\cr
+#   Named list of factor values to match rows of the data.
+# @export
 makeRefinementWrapper = function(learner, factor.values = list()) {
   learner = checkLearner(learner)
   assertList(factor.values, names = "unique")
 
   trainfun = function(data, target, args) {
-    data = data.table::as.data.table(data)
-    args = data.table::as.data.table(args)
+    data = as.data.table(data)
+    args = as.data.table(args)
     keep = setdiff(names(data), names(args))
     data = data[args, keep, on = names(args), nomatch = 0L, with = FALSE]
-    list(data = as.data.frame(data), control = list())
+    list(data = setDF(data), control = list())
   }
 
   predictfun = function(data, target, args, control) {
