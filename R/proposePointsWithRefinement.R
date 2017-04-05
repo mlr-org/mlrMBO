b610934f7d7b3fcd6908bdcc69b7910567d93649
Janek <janek.thomas@web.de>
2017-04-05 12:51:35 +0200

    fix so that it works (more or less)

--- R/proposePointsWithRefinement.R
+++ R/proposePointsWithRefinement.R
@@ -17,30 +17,32 @@ proposePointsWithRefinement = function(opt.state) {
   factor.values = as.list(res$prop.points[1L, ])
   factor.values = factor.values[!vlapply(factor.values, is.numeric)]
 
-  tasks = getOptStateTasks(opt.state)
+  tasks = getOptStateTasks(opt.state)[[1]]
   data = as.data.table(getTaskData(tasks))
   keep = setdiff(names(data), names(factor.values))
   reduced.design = data[factor.values, keep, on = names(factor.values), nomatch = 0L, with = FALSE]
+  refinement.tasks = list(mlr:::changeData(tasks, as.data.frame(reduced.design)))
 
   # change parset of smoof function
   reduced.function = fun
-  attr(fun, "") = reduced.par.set
+  attr(reduced.function, "par.set") = reduced.par.set
 
   # this is usually set in mbo/mboTemplate
   # TODO: the structure here is not good.
   refinement.control = control$refinement$control
-  refinement.control$noisy = isNoisy(fun)
-  refinement.control$minimize = shouldBeMinimized(fun)
+  refinement.control$noisy = isNoisy(reduced.function)
+  refinement.control$minimize = shouldBeMinimized(reduced.function)
+
 
   # construct the refinement learner
   refinement.learner = control$refinement$learner
-  refinement.learner = checkLearner(refinement.learner, reduced.par.set, refinement.control, fun) # TODO: checkLearner should not set stuff
+  refinement.learner = checkLearner(refinement.learner, reduced.par.set, refinement.control, reduced.function) # TODO: checkLearner should not set stuff
 
   # refinement.learner = makeRefinementWrapper(refinement.learner, factor.values = factor.values)
 
   # this is usually called in mbo/mboTemplate
-  refinement.control = checkStuff(fun, par.set, design, refinement.learner, refinement.control) # TODO: checkStuff should not set stuff
-  refinement.control$infill.crit = initCrit(refinement.control$infill.crit, fun, design, refinement.learner, refinement.control) # TODO: initCrit should not be necessary
+  refinement.control = checkStuff(reduced.function, reduced.par.set, reduced.design, refinement.learner, refinement.control) # TODO: checkStuff should not set stuff
+  refinement.control$infill.crit = initCrit(refinement.control$infill.crit, reduced.function, reduced.design, refinement.learner, refinement.control) # TODO: initCrit should not be necessary
 
   # construct new opt.state
   refinement.state = list2env(as.list(opt.state))
@@ -48,8 +50,10 @@ proposePointsWithRefinement = function(opt.state) {
   refinement.state$opt.problem$learner = refinement.learner
   refinement.state$opt.problem$control = refinement.control
   refinement.state$opt.problem$design = reduced.design
+  refinement.state$opt.problem$fun = reduced.function
   refinement.state$refinement = NULL # avoid recursion
   refinement.state$models = NULL # disable caching
+  setOptStateTasks(refinement.state, refinement.tasks)
 
   # propose with refinement
   new.res = proposePoints.OptState(refinement.state)
