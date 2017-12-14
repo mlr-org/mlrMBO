e85868ac50255a798d58c0f5b4b32f87592deda0
Jakob Richter <code@jakob-r.de>
2017-12-14 17:00:40 +0100

    documentation

--- R/convertToDesign.R
+++ R/convertToDesign.R
@@ -1,14 +1,20 @@
 # Takes x.vals and y.vals from the opt.path, possibly imputes missing x.vals and
 # returns a data.frame.
 #
+# @param opt.state [OptState]
 # @param opt.path [\code{\link[ParamHelpers]{optPath}}]\cr
 #   Optimization path.
+#   Might be altered for multipoint proposals etc.
 # @param control [\code{\link{MBOControl}}]\cr
 #   MBO control object.
+#   Might be altered for multipoint proposals etc.
 # @return [\code{data.frame}]
+#   With x and y columns
+
 convertToDesign = function(x, ...) {
   UseMethod("convertToDesign")
 }
+
 
 convertToDesign.OptState = function(opt.state, opt.path, control) {
   if (missing(opt.state) || is.null(opt.state)) {
