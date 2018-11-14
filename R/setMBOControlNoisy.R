db2239f7d6ed19ab7c981a3949161737250d539d
Jakob Richter <code@jakob-r.de>
2018-11-14 16:03:30 +0100

    make noisy repls flexible

--- R/setMBOControlNoisy.R
+++ R/setMBOControlNoisy.R
@@ -2,29 +2,17 @@
 #' @description
 #' Extends an MBO control object with options for multipoint proposal.
 #' @template arg_control
-#' @param instances [\code{integer(1)}]\cr
-#'   How many instances of one parameter will be calculated?
-#' @param instance.param [\code{character(1)}]\cr
-#'   What is the name of the function param that defines the instance?
 #' @param self.replicating [\code{logical(1)}]\cr
 #'   TRUE if the function returns a vector of noisy results for one input. Then \code{instances} specifies the length of the result we expect.
 #' @return [\code{\link{MBOControl}}].
 #' @family MBOControl
 #' @export
-setMBOControlNoisy = function(control,
-  instances = NULL,
-  instance.param = NULL,
-  self.replicating = NULL) {
+setMBOControlNoisy = function(control, self.replicating) {
 
   assertClass(control, "MBOControl")
 
-  control$noisy.instances = assertInt(instances, lower = 1L, null.ok = TRUE, na.ok = FALSE) %??% control$noisy.instances
-  control$noisy.self.replicating = assertFlag(self.replicating, null.ok = TRUE, na.ok = FALSE) %??% control$noisy.self.replicating %??% FALSE
-  control$noisy.instance.param = assertString(instance.param, null.ok = TRUE, na.ok = TRUE) %??% control$noisy.instance.param %??% ifelse(control$noisy.self.replicating, "noisy.repl", NA_character_)
-
-  if (control$noisy.self.replicating && control$noisy.instance.param != "noisy.repl") {
-    stop("You can not change the instance.param for self replicating functions.")
-  }
+  control$noisy.self.replicating = assertFlag(self.replicating %??% control$noisy.self.replicating %??% TRUE, na.ok = FALSE)
+  control$noisy.instance.param = "noisy.repl"
 
   return(control)
 }
--- tests/testthat/test_mbo_noisy.R
+++ tests/testthat/test_mbo_noisy.R
@@ -16,24 +16,6 @@ test_that("mbo works with multiple instances of noisy problems", {
   expect_true(all(table(opdf$x) == 5))
 })
 
-test_that("mbo works with multiple fixed instances of noisy problems", {
-  ps = makeNumericParamSet("x", 1, -7, 7)
-  fun = smoof::makeSingleObjectiveFunction(
-    fn = function(x) x$x^2 + rnorm(1, (x$i - 2)/100, 0.05),
-    par.set = ps,
-    noisy = TRUE,
-    has.simple.signature = FALSE
-  )
-  ctrl = makeMBOControl()
-  ctrl = setMBOControlTermination(ctrl, iters = 5L)
-  ctrl = setMBOControlInfill(ctrl, crit = crit.aei, opt.focussearch.points = 100L)
-  ctrl = setMBOControlNoisy(ctrl, instances = 5L, instance.param = "i")
-  or = mbo(fun, control = ctrl)
-  opdf = as.data.frame(or$opt.path)
-  expect_true(all(opdf$i %in% 1:5))
-  expect_true(all(table(opdf$x) == 5))
-})
-
 test_that("mbo works with self replicating instances of noisy problems", {
   ps = makeNumericParamSet("x", 1, -7, 7)
   fun = smoof::makeSingleObjectiveFunction(
@@ -44,9 +26,27 @@ test_that("mbo works with self replicating instances of noisy problems", {
   ctrl = makeMBOControl()
   ctrl = setMBOControlTermination(ctrl, iters = 5L)
   ctrl = setMBOControlInfill(ctrl, crit = crit.aei, opt.focussearch.points = 100L)
-  ctrl = setMBOControlNoisy(ctrl, instances = 5L, self.replicating = TRUE)
+  ctrl = setMBOControlNoisy(ctrl, self.replicating = TRUE)
   or = mbo(fun, control = ctrl)
   opdf = as.data.frame(or$opt.path)
   expect_true(all(opdf$noisy.repl %in% 1:5))
   expect_true(all(table(opdf$x) == 5))
+
+  # now the function has varying n results
+  i = 0
+  fun = smoof::makeSingleObjectiveFunction(
+    fn = function(x) {
+      i <<- i + 1
+      x^2 + rnorm(i, 0.01)
+    },
+    par.set = ps,
+    noisy = TRUE
+  )
+  ctrl = setMBOControlTermination(ctrl, iters = 2L)
+  ctrl = setMBOControlInfill(ctrl, crit = crit.aei, opt.focussearch.points = 100L)
+  ctrl = setMBOControlNoisy(ctrl, self.replicating = TRUE)
+  or = mbo(fun, control = ctrl)
+  opdf = as.data.frame(or$opt.path)
+  expect_true(all(opdf$noisy.repl == unlist(lapply(1:6, function(x) head(1:6,x)))))
 })
+
