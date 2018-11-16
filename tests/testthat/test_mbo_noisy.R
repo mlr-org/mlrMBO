51a6a0af3b55a4143614012171bcbffc064813a6
Jakob Richter <code@jakob-r.de>
2018-11-16 13:02:53 +0100

    fix test?

--- tests/testthat/test_mbo_noisy.R
+++ tests/testthat/test_mbo_noisy.R
@@ -26,11 +26,31 @@ test_that("mbo works with self replicating instances of noisy problems", {
     par.set = ps,
     noisy = TRUE
   )
+  ctrl = makeMBOControl()
   ctrl = setMBOControlTermination(ctrl, iters = 2L)
   ctrl = setMBOControlInfill(ctrl, crit = crit.aei, opt.focussearch.points = 100L)
   ctrl = setMBOControlNoisy(ctrl, self.replicating = TRUE)
   or = mbo(fun, control = ctrl)
   opdf = as.data.frame(or$opt.path)
   expect_true(all(opdf$noisy.repl == unlist(lapply(1:6, function(x) head(1:6,x)))))
+
+  # returns the right result
+  ps = makeNumericParamSet("x", 2, -7, 7)
+  fun = smoof::makeSingleObjectiveFunction(
+    fn = function(x) {
+      x = sum(unlist(x))
+      res = x^2 + rnorm(5, 0.01)
+      if (abs(x)>5) res[1] = -10
+      return(res)
+    },
+    par.set = ps,
+    noisy = TRUE
+  )
+  ctrl = makeMBOControl(final.method = "best.predicted")
+  ctrl = setMBOControlTermination(ctrl, iters = 5L)
+  ctrl = setMBOControlInfill(ctrl, crit = crit.aei, opt.focussearch.points = 100L)
+  ctrl = setMBOControlNoisy(ctrl, self.replicating = TRUE)
+  or = mbo(fun, control = ctrl)
+  expect_true(abs(sum(or$x$x))<5)
 })
 
