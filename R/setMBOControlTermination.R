a59bb6dbf3fafbcfed07963ef8576041f545084b
Janek Thomas <janek.thomas@web.de>
2018-11-19 17:20:39 +0100

    add assertion

--- R/setMBOControlTermination.R
+++ R/setMBOControlTermination.R
@@ -63,6 +63,7 @@
 setMBOControlTermination = function(control,
   iters = NULL, time.budget = NULL, exec.time.budget = NULL, target.fun.value = NULL, max.evals = NULL, more.termination.conds = list(), use.for.adaptive.infill = NULL) {
 
+  assertClass(control, "MBOControl")
   assertList(more.termination.conds)
   assertCharacter(use.for.adaptive.infill, null.ok = TRUE)
 
