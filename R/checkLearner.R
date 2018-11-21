8d76160f6e63083692047c7e838279d23222d5eb
Jakob Richter <code@jakob-r.de>
2018-11-21 12:09:43 +0100

    add warning + docs (#449)

--- R/checkLearner.R
+++ R/checkLearner.R
@@ -4,6 +4,9 @@ checkLearner = function(learner, control, fun) {
     learner = makeMBOLearner(control, fun, config = list(show.learner.output = FALSE, on.learner.error = control$on.surrogate.error))
   } else {
     assertClass(learner, "Learner")
+    if (!is.null(learner$config$on.learner.error) && learner$config$on.learner.error != control$on.surrogate.error) {
+      warningf("mlr setting on.learner.error = '%s' for the surrogate learner '%s' will be overwritten with mlrMBO setting on.surrogate.error = '%s'.", learner$config$on.learner.error, getLearnerId(learner), control$on.surrogate.error)
+    }
     learner$config = insert(learner$config, list(on.learner.error = control$on.surrogate.error))
   }
   # so we dont run into problems with focus search et al
