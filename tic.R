61aea201b2f517f8db56b0a4e09f741190733a9f
Jakob Richter <code@jakob-r.de>
2020-05-22 17:27:47 +0200

    revert unwanted tic change

--- tic.R
+++ tic.R
@@ -4,8 +4,6 @@ if (ci_has_env("BUILD_PKGDOWN")) {
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate")) %>%
     add_step(step_install_cran("animation")) %>%
-    add_step(step_install_cran("e1071")) %>%
-    add_step(step_install_cran("reshape2"))
-
-  do_pkgdown(orphan = TRUE)
+    add_step(step_install_cran("e1071"))
+  do_pkgdown()
 }
