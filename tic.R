464d94fa71de9c4feb9b0110095d85ed05b204c6
pat-s <patrick.schratz@gmail.com>
2020-10-05 08:05:34 +0200

    run codecov on linux

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-do_package_checks(error_on = "warning")
+do_package_checks(error_on = "warning", codedocv = FALSE)
 
 if (ci_has_env("BUILD_PKGDOWN")) {
   get_stage("install") %>%
@@ -6,4 +6,11 @@ if (ci_has_env("BUILD_PKGDOWN")) {
     add_step(step_install_cran("animation")) %>%
     add_step(step_install_cran("e1071"))
   do_pkgdown()
+}
+
+if (ci_is_env("CODECOV", "true")) {
+  get_stage("after_success") %>%
+    add_code_step(RWeka::WPM("refresh-cache")) %>%
+    add_code_step(RWeka::WPM("install-package", "XMeans")) %>%
+    add_code_step(covr::codecov())
 }
